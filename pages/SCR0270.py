6a765ec5ac739c6375047a02463ddc17905cdb92
Andrew Malone <andrew_malone@harvard.edu>
2013-07-10 13:23:50 -0400

    * Removed sizzle based locator for recent projects * Converted recent projects dropdown to base Select element * Renamed recent projects list method * Added documentation for all methods

--- pages/SCR0270.py
+++ pages/SCR0270.py
@@ -1,10 +1,9 @@
 from pages.Page import Page
-from pages.elements import Text
+from pages.elements import Text, Select
 
-# @todo: switch "recently viewed projects" dropdown to a Select element
 locators = {
     "create proposal": "css=a[href*=HomePageCreateProposalEvent]",
-    "recently viewed dropdown": "css=#s option[value='REPLACE']",
+    "recent project dropdown": "id=s",
     "recently viewed projects": "xpath=//tr[td[a[contains(@href,'ProjectListSegmentHomeEvent')]]]",
     "search": "name=projectSegmentSearchText"
 }
@@ -25,16 +24,17 @@ class recent_project():
 
 
 class SCR0270(Page):
-    """Page object for SCR_0270 GMAS Home
-
-    Attributes:
-        search: search box
+    """
+    SCR_0270 GMAS Home
     """
     locators = locators
-    search = Text("search")
+    search = Text("search", "Fund/project search box")
+    recent_project_dropdown = Select("recent project dropdown", "Dropdown for recently viewed project")
 
     def nav_to(self):
-        """Direct navigation to GMAS Home"""
+        """
+        Direct navigation to GMAS Home
+        """
         if self.get_current_page() != "SCR0270GMASHomePage":
             self.driver.get("https:%s.harvard.edu/gmas/user/SCR0270GMASHomePage.jsp" % (self.env))
             return SCR0270(self.driver)
@@ -42,25 +42,24 @@ class SCR0270(Page):
             return self
 
     def create_proposal(self):
-        """Click on the <Create an initial proposal> button
-
-        Goes to SCR_0088
+        """
+        Click on the <Create an initial proposal> button, Goes to SCR_0088
         """
         self.find("create proposal").click()
-        #from pages.SCR0088 import SCR0088
-        #return SCR0088(self.driver)
         return self.load_page()
 
-    def set_recently_viewed_projects(self, num):
-        self.find_element(locators["recently viewed dropdown"].replace("REPLACE", str(num))).click()
-        self.w.until(lambda d: d.find_element_by_css_selector("input[name=ref][value*=SCR0270]"))
-        return SCR0270(self.driver)
-
-    def get_recently_viewed_projects(self):
+    def get_recent_projects_list(self):
+        """
+        Returns a list of recently viewed projects. Each item in the list contains the following elements:
+        * project_id
+        * title
+        * link (the a element - can be clicked through webdriver)
+        * pi
+        * sponsor
+        """
         rows = self.find_elements(locators["recently viewed projects"])  # a list of tr elements
         tmp = []
         for row in rows:
             if row.is_displayed():
                 tmp.append(recent_project(row))
-        # self.recently_viewed_projects = tmp
         return tmp
