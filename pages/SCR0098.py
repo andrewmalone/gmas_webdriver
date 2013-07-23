0506bbefa1589b6dbb9e478c4184b74e7ebfc53f
Andrew Malone <andrew_malone@harvard.edu>
2013-07-23 17:02:42 -0400

    Adding inline documentation

--- pages/SCR0098.py
+++ pages/SCR0098.py
@@ -2,6 +2,9 @@ from pages.Page import Page
 
 
 class SCR0098(Page):
+    """
+    SCR_0098 RGS research team list
+    """
     locators = {
         "PI link": "link=Principal Investigator",
         "add member": "name=RequestAddResearchTeamMemberEvent",
@@ -9,14 +12,26 @@ class SCR0098(Page):
     }
 
     def add_member(self):
+        """
+        Click <Add team member>
+        Goes to SCR_0365
+        """
         self.find("add member").click()
         return self.load_page()
 
     def edit_pi(self):
+        """
+        Click the PI link
+        Goes to SCR_0365
+        """
         self.find("PI link").click()
         return self.load_page()
 
     def ok(self):
+        """
+        Click <Next>
+        Goes to SCR_0099
+        """
         self.find("next").click()
         return self.load_page()
 
--- pages/SCR0102.py
+++ pages/SCR0102.py
@@ -2,10 +2,17 @@ from pages.Page import Page
 
 
 class SCR0102(Page):
+    """
+    SCR_0102 Budget Categories
+    """
     locators = {
         "next": "name=BudgetCategoriesNextEvent"
     }
 
     def ok(self):
+        """
+        Click <Next>
+        Goes to SCR_0091, SCR_0228, or SCR_0098
+        """
         self.find("next").click()
         return self.load_page()
