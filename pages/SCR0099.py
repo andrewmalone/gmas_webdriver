bf4de5cb91824c5ba9056486f2a70647e9d73499
Andrew Malone <andrew_malone@harvard.edu>
2013-07-25 14:23:04 -0400

    Added method to add a team member

--- pages/SCR0099.py
+++ pages/SCR0099.py
@@ -6,13 +6,20 @@ class SCR0099(Page):
     SCR_0099 Request admin team (RGS)
     """
     locators = {
-        "next": "name=GrantsMgmtStaffNextEvent"
+        "next": "name=GrantsMgmtStaffNextEvent",
+        "add": "name=GrantsMgmtStaffAddTeamMemberEvent"
     }
+
+    def add_member(self):
+        """
+        Click <Add team member>
+        Goes to SCR_0230
+        """
+        return self.go("add")
 
     def ok(self):
         """
         Click <Next>
         Goes to SCR_0097
         """
-        self.find("next").click()
-        return self.load_page()
+        return self.go("next")
