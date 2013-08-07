1f39bc5f6192b0aa8ab06fe23898786b37cf7545
Andrew Malone <andrew_malone@harvard.edu>
2013-08-07 15:01:25 -0400

    Adding inline documentation

--- pages/SCR0018.py
+++ pages/SCR0018.py
@@ -2,14 +2,25 @@ from pages.Page import Page
 
 
 class SCR0018(Page):
+    """
+    SCR_0018 Indirect Cost
+    """
     locators = {
         "ok": "name=IndirectCostCalculatorOKEvent",
         "select rate": "name=IndirectCostCalculatorSelectRateEvent"
     }
 
     def select_rate(self):
+        """
+        Press the <Select rate> button
+        Goes to SCR_0020
+        """
         # to SCR_0020
         return self.go("select rate")
 
     def ok(self):
+        """
+        Press <Ok>
+        Goes to SCR_0006
+        """
         return self.go("ok")
--- pages/SCR0020.py
+++ pages/SCR0020.py
@@ -3,15 +3,24 @@ from pages.elements import Text, Select
 
 
 class SCR0020(Page):
+    """
+    SCR_0020 Indirect cost entry
+    {note}
+    Currently only handles default 1 rate for full project
+    {note}
+    """
     locators = {
         "rate": "css=input[name$='__rateValue']",
         "type": "css=select[name$='__idcTypeId']",
         "ok": "name=SelectIndirectCostRateOKEvent"
     }
 
-    rate = Text("rate")
-    rate_type = Select("type")
+    rate = Text("rate", "IDC Rate edit box")
+    rate_type = Select("type", "Rate type (dropdown)")
 
     def ok(self):
-        # to SCR_0018
+        """
+        Click <Ok>
+        Goes to SCR_0018
+        """
         return self.go("ok")
