2195666bfd97c1297fe29d35c0d27de79665b54f
Andrew Malone <andrew_malone@harvard.edu>
2013-09-05 17:09:37 -0400

    Added Global Navigation common module

--- pages/modules.py
+++ pages/modules.py
@@ -5,3 +5,11 @@ class COM0500(object):
     def goto_segment_home(self):
         self.page.find_element("css=a[href*='ProjectSnapShotSegmentHomeEvent']").click()
         return self.page.load_page()
+
+class GMAS_Header(object):
+    def __init__(self, page):
+        self.page = page
+
+    def goto_person_link(self):
+        self.page.find_element("css=a[href*='GoMyProfileEvent']").click()
+        return self.page.load_page()
