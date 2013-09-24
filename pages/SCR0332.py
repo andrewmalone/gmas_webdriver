3bc1e46e2d2befaf3f731a9a090d7e193295ef90
Andrew Malone <andrew_malone@harvard.edu>
2013-09-24 13:11:04 -0400

    Added goto_budget, renamed go_req to goto_request for consistency

--- pages/SCR0332.py
+++ pages/SCR0332.py
@@ -6,15 +6,20 @@ class SCR0332(Page):
     SCR_0332 Finish request guided steps
     """
     locators = {
-        "go request": "name=FinishGoToRequestEvent"
+        "go request": "name=FinishGoToRequestEvent",
+        "go budget": "name=FinishGoToBudgetEditBudgetEvent"
     }
 
-    def go_req(self):
+    def goto_request(self):
         """
         Click <Finish and go to request>
         Goes to SCR_0115 (through the wait screen)
         """
-        self.find("go request").click()
-        self.w.until(lambda e: len(e.find_elements_by_css_selector("script[src$='waitScreen.js']")) == 0)
-        self.w.until(lambda d: d.find_element_by_css_selector("td.footer"))
-        return self.load_page()
+        return self.go("go request")
+
+    def goto_budget(self):
+        """
+        Click <Finish & edit budget>
+        Goes to SCR_0006
+        """
+        return self.go("go budget")
