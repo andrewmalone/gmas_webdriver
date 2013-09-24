d41f6cbf6047b89b65d7771309fa5f683d518765
Andrew Malone <andrew_malone@harvard.edu>
2013-09-24 13:04:02 -0400

    Added award number

--- pages/SCR0083.py
+++ pages/SCR0083.py
@@ -6,11 +6,13 @@ class SCR0083(Page):
     locators = {
         "date issued": "name=dateIssued",
         "date received": "name=dateReceivedByHarvard",
+        "award number": "name=sponsorAwardNumber",
         "next": "name=EnterNoticeAttributesNextEvent"
     }
 
     date_issued = Text("date issued")
     date_received = Text("date received")
+    award_number = Text("award number")
 
     def ok(self):
         self.find("next").click()
