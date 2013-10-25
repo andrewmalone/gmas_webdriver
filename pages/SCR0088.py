e5648ebcd7da3788d1236af5c052445bbe1679c2
Andrew Malone <andrew_malone@harvard.edu>
2013-10-25 14:00:44 -0400

    Refactoring for better lookups

--- pages/SCR0088.py
+++ pages/SCR0088.py
@@ -1,57 +1,28 @@
 from pages.Page import Page
 from pages.elements import Text, Select, Radio
-
-locators = {
-    "select project type": "name=researchTypeId",
-    "org input": "name=org",
-    "org lookup": "css=img[alt='Lookup']",
-    "org lookup match": "css=img[name='picture'][src*='i_match.gif']",
-    "retro": "css=input[name='isRetroactiveRequest'][value='REPLACE']",
-    "next": "name=InitialProposalInstructionsNextEvent",
-    "cancel": "InitialProposalInstructionsCancelEvent"
-}
-
-
-class Lookup_org(object):
-    """
-    Org lookup - setting this value will perform the lookup (needs the 5 digit org value)
-    """
-    def __set__(self, obj, val):
-        obj._lookup_org(val)
-
-    def __get__(self, obj, type=None):
-        pass
+from pages.lookups import Lookup_org
 
 
 class SCR0088(Page):
     """
     SCR_0088 (Initial proposal instructions)
     """
+    locators = {
+        "select project type": "name=researchTypeId",
+        "org input": "name=org",
+        "retro": "css=input[name='isRetroactiveRequest'][value='REPLACE']",
+        "next": "name=InitialProposalInstructionsNextEvent",
+        "cancel": "InitialProposalInstructionsCancelEvent"
+    }
 
-    locators = locators
     project_type = Select("select project type", "Project type dropdown")
     org_text = Text("org input", "Text box for org lookup")
-    org = Lookup_org()
+    org = Lookup_org(org_text, "picture")
     retro = Radio("retro", "Retroactive yes/no question - set to true/false")
-
-    def _lookup_org(self, val):
-        """Lookup an org
-
-        Looks up an org without interacting with the popup
-
-        Args:
-            val: the 5 digit org value (string)
-        """
-        self.org_text = val
-        self.find("org lookup").click()
-        self.w.until(lambda e: self.find("org lookup match"))
-        return self
 
     def ok(self):
         """
         Clicks the <Next> button
         Goes to SCR_0089
         """
-        self.find("next").click()
-        from pages.SCR0089 import SCR0089
-        return SCR0089(self.driver)
+        return self.go("next")
--- pages/SCR0089.py
+++ pages/SCR0089.py
@@ -1,148 +1,46 @@
 from pages.Page import Page
 from pages.elements import Text, Select, Radio
-# from pages.lookups import Lookup_person, Lookup_organization
-
-locators = {
-    "title": "name=requestTitle",
-    "sponsor": "name=sponsor",
-    "sponsor lookup": "css=a[href*='sponsorValidatedImage'] img",
-    "sponsor lookup match": "css=img[name='sponsorValidatedImage'][src$='i_match.gif']",
-    "prime sponsor": "name=primeSponsor",
-    "prime sponsor lookup": "css=a[href*='primeSponsorValidatedImage'] img",
-    "pi": "name=piName",
-    "pi lookup": "css=a[href*='piImage'] img",
-    "pi lookup match": "css=img[name='piImage'][src$='i_match.gif']",
-    "mentor": "name=mentorInvestigatorName",
-    "mentor lookup": "css=a[href*='mentorValidatedImage'] img",
-    "mentor lookup match": "css=img[name='mentorValidatedImage'][src$='i_match.gif']",
-    "prime pi": "name=primePiName",
-    "prime pi lookup": "css=a[href*='primePiImage'] img",
-    "prime pi lookup match": "css=img[name='primePiImage'][src$='i_match.gif']",
-    "A21": "name=a21functionalId",
-    "discipline": "name=disciplineId",
-    "rfp": "css=input[name='isInResponseToRFP'][value='REPLACE']",
-    "subs": "css=input[name='hasSubAgreements'][value='REPLACE']",
-    "ifi": "css=input[name='isOtherSchoolSharing'][value='REPLACE']",
-    "comment": None,
-    "cancel": None,
-    "back": None,
-    "next": "name=CreateInitialRequestNextEvent"
-}
-
-
-class Lookup_pi(object):
-    """
-    Performs the PI lookup - needs an HUID
-    """
-    def __set__(self, obj, val):
-        obj._lookup_pi(val)
-
-    def __get__(self, obj, type=None):
-        pass
-
-
-class Lookup_mentor(object):
-    """
-    Performs the Mentor lookup - needs an HUID
-    """
-    def __set__(self, obj, val):
-        obj._lookup_mentor(val)
-
-    def __get__(self, obj, type=None):
-        pass
-
-class Lookup_sponsor(object):
-    """
-    Performs a search for sponsor - will select the first result from the popup window
-    """
-    def __set__(self, obj, val):
-        obj._lookup_sponsor(val)
-
-    def __get__(self, obj, type=None):
-        pass
-
-
-class Lookup_prime_sponsor(object):
-    """
-    Performs a search for prime sponsor - will select the first result from the popup window
-    """
-    def __set__(self, obj, val):
-        obj._lookup_prime_sponsor(val)
-
-    def __get__(self, obj, type=None):
-        pass
-
-
-class Lookup_prime_pi(object):
-    """
-    Performs a search for prime PI (needs HUID)
-    """
-    def __set__(self, obj, val):
-        obj._lookup_prime_pi(val)
-
-    def __get__(self, obj, type=None):
-        pass
+from pages.lookups import Lookup_person, Lookup_organization
 
 
 class SCR0089(Page):
     """
     SCR_0089
     """
-    locators = locators
+    locators = {
+        "title": "name=requestTitle",
+        "sponsor": "name=sponsor",
+        "prime sponsor": "name=primeSponsor",
+        "prime sponsor lookup": "css=a[href*='primeSponsorValidatedImage'] img",
+        "pi": "name=piName",
+        "mentor": "name=mentorInvestigatorName",
+        "prime pi": "name=primePiName",
+        "A21": "name=a21functionalId",
+        "discipline": "name=disciplineId",
+        "rfp": "css=input[name='isInResponseToRFP'][value='REPLACE']",
+        "subs": "css=input[name='hasSubAgreements'][value='REPLACE']",
+        "ifi": "css=input[name='isOtherSchoolSharing'][value='REPLACE']",
+        "comment": None,
+        "cancel": None,
+        "back": None,
+        "next": "name=CreateInitialRequestNextEvent"
+    }
     title = Text("title", "Project title")
     sponsor_text = Text("sponsor", "Sponsor name text input")
-    sponsor = Lookup_sponsor()
+    sponsor = Lookup_organization(sponsor_text, "sponsorValidatedImage", "Sponsor lookup")
     prime_sponsor_text = Text("prime sponsor", "Prime sponsor name text input")
-    prime_sponsor = Lookup_prime_sponsor()
+    prime_sponsor = Lookup_organization(prime_sponsor_text, "primeSponsorValidatedImage", "Prime sponsor lookup")
     pi_text = Text("pi", "PI name text input box")
-    pi = Lookup_pi()
+    pi = Lookup_person(pi_text, "piImage", "PI lookup")
     mentor_text = Text("mentor", "Mentor name text input box")
-    mentor = Lookup_mentor()
+    mentor = Lookup_person(mentor_text, "mentorValidatedImage", "Mentor lookup")
     prime_pi_text = Text("prime pi", "Prime PI name text input box")
-    prime_pi = Lookup_prime_pi()
+    prime_pi = Lookup_person(prime_pi_text, "primePiImage", "Prime PI lookup")
     a21 = Select("A21", "A21 dropdown")
     discipline = Select("discipline", "Discipline dropdown")
     rfp = Radio("rfp", "RFP question radio button (true/false)")
     subs = Radio("subs", "Subagreement question radio button (true/false)")
     ifi = Radio("ifi", "IFI radio button question (true/false)")
-
-    def _lookup_sponsor(self, sponsor):
-        self.sponsor_text = sponsor
-        self.find("sponsor lookup").click()
-        self.switch_to_popup()
-        from pages.SCR0536x import SCR0536x
-        popup = SCR0536x(self.driver)
-        popup.select_first_result()
-        popup.ok()
-        return self
-
-    def _lookup_prime_sponsor(self, sponsor):
-        self.prime_sponsor_text = sponsor
-        self.find("prime sponsor lookup").click()
-        self.switch_to_popup()
-        from pages.SCR0536x import SCR0536x
-        popup = SCR0536x(self.driver)
-        popup.select_first_result()
-        popup.ok()
-        return self
-
-    def _lookup_pi(self, pi):
-        self.pi_text = pi
-        self.find("pi lookup").click()
-        self.w.until(lambda e: self.find("pi lookup match"))
-        return self
-
-    def _lookup_mentor(self, mentor):
-        self.mentor_text = mentor
-        self.find("mentor lookup").click()
-        self.w.until(lambda e: self.find("mentor lookup match"))
-        return self
-
-    def _lookup_prime_pi(self, pi):
-        self.prime_pi_text = pi
-        self.find("prime pi lookup").click()
-        self.w.until(lambda e: self.find("prime pi lookup match"))
-        return self
 
     def ok(self):
         """
