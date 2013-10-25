146de55c2035c98da2ff03afa6761a7e616e0e1d
Andrew Malone <andrew_malone@harvard.edu>
2013-10-25 13:35:25 -0400

    Adding support for subagreements and IFI

--- scripts/rgs.py
+++ scripts/rgs.py
@@ -1,3 +1,13 @@
+def calc_end(start_date, num_periods):
+    """
+    helper method - sets the end date field based on the start date and the number of periods (assumes 1 year period)
+    example: calc_end("6/1/08", 2) would set the end date to 5/30/10
+    """
+    from datetime import datetime, date, timedelta
+    start = datetime.strptime(start_date, "%m/%d/%y")
+    end = date.strftime(date(start.year + int(num_periods), start.month, start.day) - timedelta(days=1), "%m/%d/%y")
+    return end
+
 def rgs(p, f=None, finish="request"):
     if f is None:
         f = {
@@ -9,13 +19,14 @@ def rgs(p, f=None, finish="request"):
             "title": "Title",
             "sponsor": "nih",
             "pi": "03750001",
+            "mentor": "",
             "prime_sponsor": "",
             "prime_pi": "",
             "a21": "A02-Organized Research",
             "discipline": "Arts",
             "rfp": "false",
-            "subs": "false",
-            "ifi": "false",
+            "subs": "true",
+            "ifi": "true",
             # SCR_0613
             "opportunity": "",
             #SCR_0231 and SCR_0231b
@@ -26,7 +37,7 @@ def rgs(p, f=None, finish="request"):
             "mailing": "Mailing instructions",  # optional
             # SCR_0090
             "start": "1/15/08",
-            "periods": "1",
+            "periods": "4",
             # SCR_0229
             "estimated_cost": "50000",  # optional
             "cost_share": "false",
@@ -34,9 +45,36 @@ def rgs(p, f=None, finish="request"):
             "admin_salary": "false",
             "program_income": "false",
             "on_campus": "true",
+            # Adding subs
+            "subagreements": [ # optional
+                {
+                    "name": "egg",
+                    "pi": "03750001"
+                },
+                {
+                    "name": "lincoln",
+                    "pi": "03750002",
+                    "start": "2/15/08", # optional
+                    "end": "3/15/08" # optional
+                }
+            ],
+            "ifi_list": [ # optional
+                {
+                    "org": "31240",
+                    "pi": "03750001"
+                },
+                {
+                    "org": "31570",
+                    "pi": "03750003",
+                    "start": "2/15/08", # optional
+                    "end": "3/15/08" # optional
+                }
+            ],
             # SCR_0098
             "pi_hs": "false",
-            "research team": [
+            "mentor_hs": "false",
+            "mentor_key": "false",
+            "research team": [ # optional
                 # {
                 #     "huid": "03750002",
                 #     "role": "Analyst",
@@ -53,7 +91,7 @@ def rgs(p, f=None, finish="request"):
                 # }
             ],
             # SCR_0099
-            "admin_team": [
+            "admin_team": [ # optional
                 # {
                 #     "huid": "03750002",
                 #     "role": "Department Administrator"
@@ -86,7 +124,9 @@ def rgs(p, f=None, finish="request"):
     p.title = f["title"]
     p.sponsor = f["sponsor"]
     p.pi = f["pi"]
-    if f["prime_sponsor"] != "":
+    if "mentor" in f and f["mentor"] != "" and f["project_type"] == "Fellowship":
+        p.mentor = f["mentor"]
+    if "prime_sponsor" in f and f["prime_sponsor"] != "":
         p.prime_sponsor = f["prime_sponsor"]
         p.prime_pi = f["prime_pi"]
     p.a21 = f["a21"]
@@ -98,7 +138,7 @@ def rgs(p, f=None, finish="request"):
 
     # Need to check for which screen... (231 or 613)
     if p.get_current_page() == "SCR0613EnterOpportunity":
-        if f["opportunity"] != "":
+        if "opportunity" in f and f["opportunity"] != "":
             p.opportunity = f["opportunity"]
         p = p.ok()
 
@@ -138,20 +178,62 @@ def rgs(p, f=None, finish="request"):
     p = p.ok()
 
     # SUBS and IFI get added here...
+    # SCR_0091
+    if f["subs"] == "true":
+        if "subagreements" in f:
+            for sub in f["subagreements"]:
+                p = p.add_sub()
+                # SCR_0092
+                p.sub_name = sub["name"]
+                p.sub_pi = sub["pi"]
+                if "start" in sub and "end" in sub:
+                    p.start = sub["start"]
+                    p.end = sub["end"]
+                else:
+                    p.start = f["start"]
+                    p.end = calc_end(f["start"], f["periods"])
+                p = p.ok()
+        p = p.ok()
+
+    # SCR_0228
+    if f["ifi"] == "true":
+        if "ifi_list" in f:
+            for ifi in f["ifi_list"]:
+                p = p.add_ifi()
+                # SCR_0094
+                p.org = ifi["org"]
+                p.pi = ifi["pi"]
+                if "start" in ifi and "end" in ifi:
+                    p.start = ifi["start"]
+                    p.end = ifi["end"]
+                else:
+                    p.start = f["start"]
+                    p.end = calc_end(f["start"], f["periods"])
+                p = p.ok()
+        p = p.ok()
+
 
     # SCR_0098 (and SCR_0365)
     p = p.edit_pi()
     p.human_subjects = f["pi_hs"]
     p = p.ok()
-    for person in f["research team"]:
-        p = p.add_member()
-        p.role = person["role"]
-        p.lookup_person(person["huid"])
-        p.key = person["key"]
-        p.human_subjects = person["hs"]
-        if person["key"] == "false" and p.investigator is True:
-            p.investigator = person["investigator"]
+    if f["project_type"] == "Fellowship":
+        p = p.edit_mentor()
+        p.human_subjects = f["mentor_hs"]
+        p.key = f["mentor_key"]
+        if f["mentor_key"] == "false" and p.investigator is True:
+            p.investigator = f["mentor_investigator"]
         p = p.ok()
+    if "research team" in f:
+        for person in f["research team"]:
+            p = p.add_member()
+            p.role = person["role"]
+            p.lookup_person(person["huid"])
+            p.key = person["key"]
+            p.human_subjects = person["hs"]
+            if person["key"] == "false" and p.investigator is True:
+                p.investigator = person["investigator"]
+            p = p.ok()
 
     p = p.ok()
 
@@ -195,3 +277,8 @@ def rgs(p, f=None, finish="request"):
         p = p.goto_request()
 
     return p
+
+if __name__ == "__main__":
+    from gmas_webdriver.setup import init
+    p = init("Firefox", "gmasdev.cadm", True)
+    p = rgs(p)
