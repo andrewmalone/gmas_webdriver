6fc25113491c721d18b24ccc78fedd14a6039b32
Whitney Cross <whitney_cross@harvard.edu>
2013-11-06 11:31:51 -0500

    Added new locators and methods for input and search.

--- pages/SCR0067.py
+++ pages/SCR0067.py
@@ -1,8 +1,24 @@
 from pages.Page import Page
-
-locators = {
-
-}
+from pages.elements import Text
 
 class SCR0067(Page):
-	pass
+    """
+    SCR_0067 Organization search
+    """
+    locators = {
+	"org_name": "name=organizationSearchString",
+	"search": "name=MainOrganizationSearchEvent",
+        "org link": "css=a[href*='ViewOrganizationDetailsEvent']"
+    }
+
+    org_name = Text("org_name", "Text entry for new name")
+
+    def search(self):
+        """
+        Clicks <Search>
+        Stays on SCR0067
+        """
+        return self.go("search")
+
+    def goto_first_org(self):
+        return self.go("org link")
