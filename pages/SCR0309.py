91103af207dac2a63f7952dc13d4e48192473a4d
Whitney Cross <whitney_cross@harvard.edu>
2013-11-06 16:43:37 -0500

    Added new documents methods

--- pages/SCR0309.py
+++ pages/SCR0309.py
@@ -2,9 +2,13 @@ from pages.Page import Page
 
 
 class SCR0309(Page):
+    """
+    SCR_0309 Notice Home
+    """
     locators = {
         "review completed": "name=ReviewCompletedEvent",
-        "revise project": "name=CreateNewRevisionEvent"
+        "revise project": "name=CreateNewRevisionEvent",
+        "documents": "link=Documents"
     }
 
     def review_completed(self):
@@ -12,3 +16,10 @@ class SCR0309(Page):
 
     def revise_project(self):
         return self.go("revise project")
+
+    def goto_documents(self):
+        """
+        Clicks the "Documents" link
+        Goes to SCR_0433
+        """
+        return self.go("documents")
