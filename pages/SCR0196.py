51ccdf27bef0ca725bc9cf374b82131c282c38b7
Andrew Malone <andrew_malone@harvard.edu>
2013-12-24 08:43:54 -0500

    Added row subclass for handling multiple accounts

--- pages/SCR0196.py
+++ pages/SCR0196.py
@@ -1,26 +1,83 @@
-from pages.Page import Page
-
-locators = {
-    "add account": "name=AddAccountRevisionEvent",
-    "ok": "name=EditAccountsSaveChangesEvent",
-    "edit account": "link=REPLACE"
-}
-
+from pages.Page import Page, GMWebElement
+from pages.elements import Checkbox
 
 class SCR0196(Page):
-    locators = locators
+    """
+    SCR_0196 Edit accounts in revision
+    """
+    locators = {
+        "add account": "name=AddAccountRevisionEvent",
+        "ok": "name=EditAccountsSaveChangesEvent",
+        "account name": "link=REPLACE",
+        "account": "css=a[href*=viewDescription]",
+        "account row": "xpath=//a[contains(@href,'viewDescription')]/../..",
+        "next": "EditAccountsNextEvent"
+    }
+
+    class account_row(GMWebElement):
+        """
+        Subclass for an individual account row on SCR_0196
+        """
+        locators = {
+            "checkbox": "css=input[type=checkbox]",
+            "link": "css=a[href*=viewDescription]"
+        }
+
+        checkbox = Checkbox("checkbox", "Checkbox to send a validated account to the GL")
+
+        def __init__(self, row, page):
+            self.driver = row
+            self.page = page
+
+        def go(self):
+            """
+            Click the account's link
+            Goes to SCR_0474
+            """
+            self.find("link").click()
+            return self.page.load_page()
+
+    def account(self, n):
+        """
+        Returns the nth account row object:
+        //account_row
+
+        Example: Edit the second account on the screen - `p = p.account(2).go()`
+        """
+        return self.account_row(self.finds("account row")[n - 1], self)
 
     def add_account(self):
-        self.find_element(locators["add account"]).click()
-        from pages.SCR0474 import SCR0474
-        return SCR0474(self.driver)
+        """
+        Click <Add account>
+        Goes to SCR_0474
+        """
+        return self.go("add account")
 
     def ok(self):
-        self.find_element(locators["ok"]).click()
-        from pages.SCR0105 import SCR0105
-        return SCR0105(self.driver)
+        """
+        Click <Done making changes to this section>
+        Goes to SCR_0105
+        """
+        return self.go("ok")
 
-    def edit_account(self, account_name):
-        self.find("edit account", account_name).click()
-        from pages.SCR0474 import SCR0474
-        return SCR0474(self.driver)
+    def next(self):
+        """
+        Click <Next>
+        Goes to SCR_0427
+        """
+        return self.go("next")
+
+    def edit_account(self, account):
+        """
+        Click an account link. `account` can be a string or a positive integer
+        * string: clicks the link matching the passed string `p = p.edit_account('Main 1')`
+        * integer: clicks the nth account link (counting starts at 1). `p = p.edit_account(2)` clicks the second account on the page
+
+        Goes to SCR_0474
+        """
+        if type(account) is str:
+            return self.go("account name", account)
+        if type(account) is int and account > 0:
+            self.finds("account")[account - 1].click()
+            return self.load_page()
+
