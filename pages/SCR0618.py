ffbf4f67c8bd39e098995bfbdad66bdae5b2fc06
Andrew Malone <andrew_malone@harvard.edu>
2014-01-30 10:28:02 -0500

    Adding support for reading values from SCR_0618

--- pages/SCR0618.py
+++ pages/SCR0618.py
@@ -1,8 +1,16 @@
 from pages.Page import Page
-
+from pages.elements import RText
+import utilities.xpath as xpath
 
 class SCR0618(Page):
     """
     SCR_0618
     """
-    pass
+    locators = {
+        "update": "GrantsGovSubmissionHistoryGetUpdateEvent",
+        "tracking": "xpath=%s" % xpath.xpath_text_sibling("td", "Grants.gov tracking no.", 2),
+        "status": "xpath=%s" % xpath.xpath_text_sibling("td", "Submission status", 2)
+    }
+
+    tracking = RText("tracking", "Grants.gov tracking number")
+    status = RText("status", "Grants.gov status")
