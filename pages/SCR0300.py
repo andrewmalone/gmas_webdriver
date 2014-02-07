2ef3e4d31d2e64a3b7b98bb5df1a4701a1697d4a
Andrew Malone <andrew_malone@harvard.edu>
2014-02-07 09:20:21 -0500

    Support for interacting with roles or people

--- pages/SCR0300.py
+++ pages/SCR0300.py
@@ -1,12 +1,15 @@
 from pages.Page import Page
-
+from pages.elements import Row, RText
 
 class SCR0300(Page):
     """
     SCR_0300 Admin team
     """
     locators = {
-        "role": "link=REPLACE"
+        "role": "link=REPLACE",
+        "row by person": "xpath=//a[contains(@href,'ProjectAdminTeamPersonProfileEvent')][contains(@href,'personId=REPLACE')]/ancestor::tr[1]",
+        "row by role": "xpath=//*[contains(text(),'REPLACE')][not(@class)]/ancestor::tr[1]",
+        "add": "ProjectAdminTeamAddRolesEvent"
     }
 
     def goto_role(self, role):
@@ -15,3 +18,45 @@ class SCR0300(Page):
         Goes to SCR_0301
         """
         return self.go("role", role)
+
+    def role_rows(self, role):
+        elements = self.finds("row by role", role)
+        rows = []
+        for el in elements:
+            rows.append(self.role_row(el, self))
+        return rows
+
+    def person_rows(self, person_id):
+        elements = self.finds("row by person", person_id)
+        rows = []
+        for el in elements:
+            rows.append(self.role_row(el, self))
+        return rows
+
+    def add_role(self):
+        """
+        Click <Add roles>
+        Goes to SCR_0416
+        """
+        return self.go("add")
+
+    class role_row(Row):
+        locators = {
+            "role": "css=td:nth-child(3)",
+            "name": "css=td:nth-child(7)",
+            "role link": "css=a[href*=ProjectAdminTeamEditRolesEvent]",
+            "name link": "css=a[href*=ProjectAdminTeamPersonProfileEvent]"
+        }
+
+        role = RText("role")
+        name = RText("name")
+
+        def goto_role(self):
+            return self._go("role link")
+
+        def person_id(self):
+            import urlparse
+            element = self.find("name link")
+            url = urlparse.urlparse(element.get_attribute("href"))
+            query = urlparse.parse_qs(url.query)
+            return query["personId"][0]
