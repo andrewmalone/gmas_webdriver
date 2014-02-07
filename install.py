3cf4ba56d70f8fd8bb9ae4c28eb20e97af545e56
Andrew Malone <andrew_malone@harvard.edu>
2014-02-07 09:23:33 -0500

    Adding database username/password

--- install.py
+++ install.py
@@ -6,10 +6,14 @@ config = ConfigParser.RawConfigParser()
 
 huid = base64.b64encode(str(input("Enter your HUID: ")))
 pin = base64.b64encode(str(getpass.getpass("Enter your PIN: ")))
+db_user = base64.b64encode(str(input("Enter your database user: ")))
+db_pass = base64.b64encode(str(getpass.getpass("Enter your database password: ")))
 
 config.add_section("setup")
 config.set("setup", "a", huid)
 config.set("setup", "b", pin)
+config.set("setup", "c", db_user)
+config.set("setup", "d", db_pass)
 
 with open('config.ini', 'wb') as configfile:
     config.write(configfile)
