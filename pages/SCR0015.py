eedf3317da301c69ab1ab272be110163f44278b9
Andrew Malone <andrew_malone@harvard.edu>
2014-02-26 14:41:51 -0500

    Refactored to newer methods. Added method to follow name or role link

--- pages/SCR0015.py
+++ pages/SCR0015.py
@@ -1,46 +1,50 @@
 from pages.Page import Page
-
-locators = {
-    "add team member": "name=AddTeamMemberEvent",
-    "PI link": "link=Principal Investigator",
-    "edit team": "name=EditResearchTeamButton"
-}
+import utilities.xpath as xpath
 
 
 class SCR0015(Page):
     """
     SCR_0015 Research team list
-    {note}
+    
     This has only been tested with the request version of the screen. Also, there's no great way to go to a person by name (will be added eventually)
-    {note}
     """
-    locators = locators
+    locators = {
+        "add team member": "name=AddTeamMemberEvent",
+        "PI link": "link=Principal Investigator",
+        "edit team": "name=EditResearchTeamButton",
+        "person row": xpath.parent_row_of_event("ResearchPersonNameLinkEvent"),
+        "person links": "event=ResearchPersonNameLinkEvent",
+        "role links": "event=ResearchTeamMemberViewEvent"
+    }
 
     def add_team_member(self):
         """
         Click <Add team member>
         Goes to SCR_0252
         """
-        self.find_element(locators["add team member"]).click()
-        from pages.SCR0252 import SCR0252
-        return SCR0252(self.driver)
+        return self.go("add team member")
 
     def goto_pi(self):
         """
         Click the "Principal investigator" link
         Goes to SCR_0363
         """
-        self.find("PI link").click()
+        return self.go("PI link")
+
+    def goto_name(self, num):
+        """
+        Click the nth name on the screen
+        Goes to SCR_0025
+        """
+        self.finds("person links")[num - 1].click()
         return self.load_page()
 
-    def goto_member(self, num):
+    def goto_role(self, num):
         """
-        Click the research team link for the nth entry in the list (0 based)
+        Click the nth role on the screen
         Goes to SCR_0363
         """
-        # ResearchTeamMemberViewEvent
-        elems = self.find_elements("css=a[href*='ResearchTeamMemberViewEvent']")
-        elems[num].click()
+        self.finds("role links")[num - 1].click()
         return self.load_page()
 
     def edit_team(self):
