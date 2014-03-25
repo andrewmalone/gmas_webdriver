81916a0cd928b7c85907a659ec35286158b1b376
andrewmalone <andrew@andrewmalone.com>
2014-03-25 15:58:50 -0400

    Select a request row based on request id

--- pages/SCR0344.py
+++ pages/SCR0344.py
@@ -1,21 +1,28 @@
 from pages.Page import Page
 from pages.elements import Row
 
+
 class SCR0344(Page):
     """
     SCR_0344 Request list
     """
     locators = {
-        "request row": "xpath=//a[contains(@href,'RequestListLinkEvent')]/ancestor::tr[1]"
+        "request row": "xpath=//a[contains(@href,'RequestListLinkEvent')]/ancestor::tr[1]",
+        "request row id": "xpath=//a[contains(@href,'requestId=REPLACE')]/ancestor::tr[1]"
     }
 
-    def requests(self, num):
+    def request(self, num=None, request_id=None):
         """
-        returns a request row based on the num index (1 indexed)
+        returns a request row based on index or id
+        `p.request(1)` returns the first request
+        `p.request(request_id=12345)` returns the request row for request id 12345
         //request_row
         """
-        return self.request_row(self.finds("request row")[num - 1], self)
-
+        if request_id is None:
+            row = self.finds("request row")[num - 1]
+        else:
+            row = self.find("request row id", request_id)
+        return self.request_row(row, self)
 
     class request_row(Row):
         locators = {
