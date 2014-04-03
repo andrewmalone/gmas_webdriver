b8cba33c729c31c124b7528ec4125e7ea7fa1733
andrewmalone <andrew@andrewmalone.com>
2014-04-03 09:53:56 -0400

    Added count property

--- pages/SCR0634.py
+++ pages/SCR0634.py
@@ -6,5 +6,12 @@ class SCR0634(Page):
     SCR_0634 Segment documents
     """
     locators = {
-    
+        "document": "event=DocumentActionViewEvent"
     }
+
+    @property
+    def document_count(self):
+        """
+        Number of documents shown
+        """
+        return len(self.finds("document"))
