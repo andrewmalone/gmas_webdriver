7bb524f4b02521bfceeaf75cf335a3c3ba9c0a98
andrewmalone <andrew@andrewmalone.com>
2014-05-08 15:48:35 -0400

    PEP8

--- pages/SCR0250.py
+++ pages/SCR0250.py
@@ -8,37 +8,36 @@ class SCR0250(Page):
     SCR_0250 Subagreement list for request
     """
     locators = {
-    	"sub link": "event=ViewSubagreementEvent",
-    	"sub row": xpath.parent_row_of_event("ViewSubagreementEvent")
+        "sub link": "event=SubagreementEvent",
+        "sub row": xpath.parent_row_of_event("SubagreementEvent")
     }
 
     @property
     def sub_count(self):
-    	"""
-    	Count of subagreements shown on the page
-    	"""
-    	return len(self.finds("sub link"))
+        """
+        Count of subagreements shown on the page
+        """
+        return len(self.finds("sub link"))
 
     def sub(self, n):
-    	"""
-    	Returns the row for the nth sub on the page
-    	//Subagreement_row
-    	"""
-    	row = self.finds("sub row")[n - 1]
-    	return self.Subagreement_row(row, self)
+        """
+        Returns the row for the nth sub on the page
+        //Subagreement_row
+        """
+        row = self.finds("sub row")[n - 1]
+        return self.Subagreement_row(row, self)
 
     class Subagreement_row(Row):
-    	locators = {
-    		"link": "event=ViewSubagreementEvent",
-    		"status": "css=td:nth-child(11)"
-    	}
+        locators = {
+            "link": "event=SubagreementEvent",
+            "status": "css=td:nth-child(11)"
+        }
 
-    	status = RText("status", "Subagreement status")
+        status = RText("status", "Subagreement status")
 
-    	def go(self):
-    		"""
-    		Clicks the subagreement link
-    		Goes to SCR_0450 or SCR_0233
-    		"""
-    		return self._go("link")
-    
+        def go(self):
+            """
+            Clicks the subagreement link
+            Goes to SCR_0450 or SCR_0233
+            """
+            return self._go("link")
--- pages/SCR0450.py
+++ pages/SCR0450.py
@@ -2,16 +2,16 @@ from pages.Page import Page
 
 
 class SCR0450(Page):
-	"""
-	SCR_0450 Sub under development
-	"""
-	locators = {
-		"budget": "event=ViewSubagreementDetailBudgetEvent"
-	}
+    """
+    SCR_0450 Sub under development
+    """
+    locators = {
+        "budget": "event=ViewSubagreementDetailBudgetEvent"
+    }
 
-	def goto_budget(self):
-		"""
-		Clicks the <Subagreement detailed budget> button
-		Goes to SCR_0428
-		"""
-		return self.go("budget")
+    def goto_budget(self):
+        """
+        Clicks the <Subagreement detailed budget> button
+        Goes to SCR_0428
+        """
+        return self.go("budget")
