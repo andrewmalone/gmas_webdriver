f6951e32cd9075363c1acda59582542bfd63b04a
andrewmalone <andrew@andrewmalone.com>
2014-05-08 15:51:12 -0400

    Sub admin and IDC rates in RGS

--- pages/SCR0092.py
+++ pages/SCR0092.py
@@ -1,6 +1,7 @@
 from pages.Page import Page
-from pages.elements import Text
+from pages.elements import Text, Row, Select
 from pages.lookups import Lookup_person, Lookup_organization
+
 
 class SCR0092(Page):
     """
@@ -9,19 +10,30 @@ class SCR0092(Page):
     locators = {
         "sub name text": "name=subrecipientName",
         "sub pi text": "name=vendorPIName",
+        "admin text": "vendorAdminName",
         "start": "name=proposedStartDate",
         "end": "name=proposedEndDate",
-        "ok": "name=SubagreementDetailsOKEvent"
+        "ok": "name=SubagreementDetailsOKEvent",
+        "description": "description",
+        "rate": "xpath=//input[contains(@name, 'idcRateVersion')]//ancestor::tr[1]",
+        "add rate": "RequestAddSubrecipientRateEvent",
+        "basis": "indirectBasisTypeId"
     }
 
-    sub_name_text = Text("sub name text", "subagreement name text input")
+    sub_name_text = Text("sub name text", "subagreement name")
     sub_name = Lookup_organization(sub_name_text, "orgLookupImage", "Subagreement organization")
 
-    sub_pi_text = Text("sub pi text", "subagreement PI text input")
+    sub_pi_text = Text("sub pi text", "subagreement PI")
     sub_pi = Lookup_person(sub_pi_text, "vendorPILookupImage", "Subagreement PI")
+
+    sub_admin_text = Text("admin text", "Sub admin contact")
+    sub_admin = Lookup_person(sub_admin_text, "administratorImage", "Sub admin contact")
 
     start = Text("start", "Subagreement start date")
     end = Text("end", "Subagreement end date")
+
+    description = Text("description", "Description")
+    basis = Select("basis", "Indirect basis")
 
     def ok(self):
         """
@@ -29,4 +41,26 @@ class SCR0092(Page):
         Goes to SCR_0091
         """
         return self.go("ok")
+
+    def rate(self, n):
+        """
+        Nth Sub IDC rate rows
+        """
+        row = self.finds("rate")[n - 1]
+        return self.Rate_row(row, self)
+
+    def add_rate(self):
+        """
+        Click <Add rate>
+        """
+        return self.go("add rate")
+
+    class Rate_row(Row):
+        locators = {
+            "rate": "css=input[type=text][name^=rate]",
+            "date": "css=input[type=text][name^=expirationDate"
+        }
+
+        rate = Text("rate", "Indirect rate")
+        expiration = Text("date", "Indirect expiration")
 
