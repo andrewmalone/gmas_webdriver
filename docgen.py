051ea433a8b54105f605881c3d46130142c13ebc
andrewmalone <andrew@andrewmalone.com>
2014-05-08 15:53:05 -0400

    ugh. tabs to spaces

--- docgen.py
+++ docgen.py
@@ -11,16 +11,16 @@ path = "pages/"
 savepath = "../gmas_webdriver_wiki/"
 
 for f in os.listdir(path):
-	if "SCR" in f and f[-2:] == "py":
-		# get the filename/classname (f[:-3])
-		doc = ""
-		try:
-			doc = docgen.get_method_list(f[:-3])
-		except:
-			print "ERROR %s" % f[:-3]
+    if "SCR" in f and f[-2:] == "py":
+        # get the filename/classname (f[:-3])
+        doc = ""
+        try:
+            doc = docgen.get_method_list(f[:-3])
+        except:
+            print "ERROR %s" % f[:-3]
 
-		if doc != "":
-			with open("%s%s.md" % (savepath, f[:-3]), "w") as file:
-				file.write(doc)
+        if doc != "":
+            with open("%s%s.md" % (savepath, f[:-3]), "w") as file:
+                file.write(doc)
 
 # @todo - generate index page for page objects...
