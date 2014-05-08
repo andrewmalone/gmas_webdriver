089022606aa37213ab2d73a8a96c0a45141b5883
andrewmalone <andrew@andrewmalone.com>
2014-05-08 15:49:34 -0400

    Support for radio buttons

--- pages/SCR0102.py
+++ pages/SCR0102.py
@@ -1,4 +1,16 @@
 from pages.Page import Page
+from pages.elements import Radio
+
+
+template_map = {
+    "army": "9109",
+    "energy": "9105",
+    "ggov": "9113",
+    "standard": "9110",
+    "nasa": "9107",
+    "nih": "9101",
+    "nsf": "9104"
+}
 
 
 class SCR0102(Page):
@@ -6,13 +18,15 @@ class SCR0102(Page):
     SCR_0102 Budget Categories
     """
     locators = {
-        "next": "name=BudgetCategoriesNextEvent"
+        "next": "name=BudgetCategoriesNextEvent",
+        "template": "css=input[name=sponsorBudgetTemplate]"
     }
+
+    template = Radio("template", "Budget template", mapping=template_map)
 
     def ok(self):
         """
         Click <Next>
         Goes to SCR_0091, SCR_0228, or SCR_0098
         """
-        self.find("next").click()
-        return self.load_page()
+        return self.go("next")
