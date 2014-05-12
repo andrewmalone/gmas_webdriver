e1c491385784c0f5cb837eb0c09bd6bcd8674ee2
andrewmalone <andrew@andrewmalone.com>
2014-05-12 16:10:04 -0400

    Refactoring last 5 projects
    Use Row class instead of custom class

--- pages/SCR0270.py
+++ pages/SCR0270.py
@@ -1,38 +1,23 @@
 from pages.Page import Page
-from pages.elements import Text, Select
-
-locators = {
-    "create proposal": "css=a[href*=HomePageCreateProposalEvent]",
-    "recent project dropdown": "id=s",
-    "recently viewed projects": "xpath=//tr[td[a[contains(@href,'ProjectListSegmentHomeEvent')]]]",
-    "search box": "name=projectSegmentSearchText",
-    "search": "name=ProjectSearchEvent",
-    "loc": "link=Letter of Credit",
-    "documents": "name=g_documentsoff",
-    "organizations": "name=g_organizations",
-    "standing teams": "event=ViewStandingTeamEvent"
-}
-
-
-class recent_project():
-    def __init__(self, row):
-        cells = row.find_elements_by_tag_name("td")
-        # 1 = project id
-        # 5 = link (from title)
-        # 9 = PI Name
-        # 13 = Sponsor name
-        self.project_id = cells[1].text
-        self.title = cells[5].text
-        self.link = cells[5].find_element_by_tag_name("a")
-        self.pi = cells[9].text
-        self.sponsor = cells[13].text
+from pages.elements import Text, Select, Row
 
 
 class SCR0270(Page):
     """
     SCR_0270 GMAS Home
     """
-    locators = locators
+    locators = {
+        "create proposal": "css=a[href*=HomePageCreateProposalEvent]",
+        "recent project dropdown": "id=s",
+        "recently viewed projects": "xpath=//tr[td[a[contains(@href,'ProjectListSegmentHomeEvent')]]]",
+        "search box": "name=projectSegmentSearchText",
+        "search": "name=ProjectSearchEvent",
+        "loc": "link=Letter of Credit",
+        "documents": "name=g_documentsoff",
+        "organizations": "name=g_organizations",
+        "standing teams": "event=ViewStandingTeamEvent"
+    }
+
     search_text = Text("search box", "Fund/project search box")
     recent_project_dropdown = Select("recent project dropdown", "Dropdown for recently viewed project")
 
@@ -50,24 +35,15 @@ class SCR0270(Page):
         """
         Click on the <Create an initial proposal> button, Goes to SCR_0088
         """
-        self.find("create proposal").click()
-        return self.load_page()
+        return self.go("create proposal")
 
-    def get_recent_projects_list(self):
+    def recent_project(self, n):
         """
-        Returns a list of recently viewed projects. Each item in the list contains the following elements:
-        * project_id
-        * title
-        * link (the a element - can be clicked through webdriver)
-        * pi
-        * sponsor
+        Returns the nth row from the recent projects list
+        //Recent_project
         """
-        rows = self.find_elements(locators["recently viewed projects"])  # a list of tr elements
-        tmp = []
-        for row in rows:
-            if row.is_displayed():
-                tmp.append(recent_project(row))
-        return tmp
+        row = self.finds("recently viewed projects")[n - 1]
+        return self.Recent_project(row, self)
 
     def search(self):
         """
@@ -88,7 +64,7 @@ class SCR0270(Page):
         Clicks the "Documents" link
         Goes to SCR_0139
         """
-        return self.go("documents")  
+        return self.go("documents")
 
     def goto_organizations(self):
         """
@@ -103,3 +79,26 @@ class SCR0270(Page):
         Goes to SCR_0066
         """
         return self.go("standing teams")
+
+    class Recent_project(Row):
+        # def __init__(self, row):
+        #     cells = row.find_elements_by_tag_name("td")
+        #     # 1 = project id
+        #     # 5 = link (from title)
+        #     # 9 = PI Name
+        #     # 13 = Sponsor name
+        #     self.project_id = cells[1].text
+        #     self.title = cells[5].text
+        #     self.link = cells[5].find_element_by_tag_name("a")
+        #     self.pi = cells[9].text
+        #     self.sponsor = cells[13].text
+        locators = {
+            "link": "css=a"
+        }
+
+        def go(self):
+            """
+            Clicks a recent project link
+            Goes to SCR_0104b
+            """
+            return self._go("link")
