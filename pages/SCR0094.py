5d77b03b00e8e3ee33762b74295302f0cd05cfd1
andrewmalone <andrew@andrewmalone.com>
2014-05-12 16:09:44 -0400

    Adding IFI admin

--- pages/SCR0094.py
+++ pages/SCR0094.py
@@ -2,6 +2,7 @@ from pages.Page import Page
 from pages.elements import Text
 from pages.lookups import Lookup_org, Lookup_person
 
+
 class SCR0094(Page):
     """
     SCR_0094 Add new IFI in RGS
@@ -9,6 +10,7 @@ class SCR0094(Page):
     locators = {
         "org": "name=orgDescription",
         "pi": "name=personName",
+        "admin": "administratorName",
         "start": "name=involvementStartDate",
         "end": "name=involvementEndDate",
         "ok": "name=MultiTubMultiOrgOKEvent"
@@ -16,10 +18,12 @@ class SCR0094(Page):
 
     org_text = Text("org", "Input box for the Org")
     pi_text = Text("pi", "Input box for the IFI PI")
+    admin_text = Text("admin", "Input box for the IFI Admin")
     start = Text("start", "IFI start date")
     end = Text("end", "IFI end date")
     org = Lookup_org(org_text, "picture", "IFI Org")
     pi = Lookup_person(pi_text, "piImage", "IFI PI")
+    admin = Lookup_person(admin_text, "administratorImage", "IFI Admin")
 
     def ok(self):
         """
