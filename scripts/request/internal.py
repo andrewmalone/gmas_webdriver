4c553ab49afc868abd420f810fa0db9a50f5d428
andrewmalone <andrew@andrewmalone.com>
2014-05-21 09:42:02 -0400

    adding admin change request

--- scripts/request/internal.py
+++ scripts/request/internal.py
@@ -33,3 +33,47 @@ def create_internal(p, f={}):
 
     return p
 
+
+def create_admin(p, f={}):
+    """
+    Starts from a segment home and creates a changes to existing segment request
+    {
+        "preaward": True,
+        "carryforward": False,
+        "nocost": False,
+        "equipment": False,
+        "sub": False,
+        "ifi": False,
+        "changepi": False,
+        "rebudget": False,
+        "termination": False,
+    }
+    """
+    if p.scr != "SCR0104":
+        raise Exception("Not on segment home")
+
+    p = p.create_request()
+
+    # SCR_0472
+    p.request_type = "Changes to existing segment"
+    p.retro = f["retro"] if "retro" in f else "false"
+    p = p.ok()
+
+    # SCR_0541
+    p.preaward = f["preaward"] if "preaward" in f else False
+    p.carryforward = f["carryforward"] if "carryforward" in f else False
+    p.nocost = f["nocost"] if "nocost" in f else False
+    p.equipment = f["equipment"] if "equipment" in f else False
+    p.sub = f["sub"] if "sub" in f else False
+    p.ifi = f["ifi"] if "ifi" in f else False
+    p.changepi = f["changepi"] if "changepi" in f else False
+    p.rebudget = f["rebudget"] if "rebudget" in f else False
+    p.termination = f["termination"] if "termination" in f else False
+    p = p.ok()
+
+    # SCR_0465
+    if "justification" in f:
+        p.justification = f["justification"]
+    p = p.ok()
+
+    return p
