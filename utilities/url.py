ef1ad93fd7abb5aed37fde203e497d570d4fffe9
andrewmalone <andrew@andrewmalone.com>
2014-05-21 09:40:54 -0400

    Handle cases where the requested param doesn't exist
    Don’t throw an exception

--- utilities/url.py
+++ utilities/url.py
@@ -1,9 +1,13 @@
 import urlparse
 
+
 def url_param(url, param):
     """
     Returns the value of a given url paramater
     """
     url = urlparse.urlparse(url)
     query = urlparse.parse_qs(url.query)
-    return query[param][0]
+    if param in query:
+        return query[param][0]
+    else:
+        return None
