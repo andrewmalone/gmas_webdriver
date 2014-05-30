e657f06c265153142d43ecc80102f41eb7143536
andrewmalone <andrew@andrewmalone.com>
2014-05-30 11:28:24 -0400

    Support for paging through budget periods

--- pages/SCR0410.py
+++ pages/SCR0410.py
@@ -6,8 +6,24 @@ class SCR0410(Page):
     SCR_0410 Indirect Costs
     """
     locators = {
-        "excluded": "event=ViewIndirectCostViewExclusionsEvent"
+        "excluded": "event=ViewIndirectCostViewExclusionsEvent",
+        "next period": "css=input[title='Next period']",
+        "periods": "xpath=//td[@class='strong'][contains(text(),'Period')]"
     }
+
+    @property
+    def period_count(self):
+        """
+        Total number of budget periods
+        """
+        return self.find("periods").text[12]
+
+    @property
+    def current_period(self):
+        """
+        Current budget period
+        """
+        return self.find("periods").text[7]
 
     def goto_exclusions(self):
         """
@@ -15,3 +31,9 @@ class SCR0410(Page):
         Goes to SCR_0164
         """
         return self.go("excluded")
+
+    def next(self):
+        """
+        Click the next period button
+        """
+        return self.go("next period")
