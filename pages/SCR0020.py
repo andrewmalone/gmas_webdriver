fc8e36b11ce8abe11fdb12d715cca7e9a0bb1bc9
andrewmalone <andrew@andrewmalone.com>
2014-05-30 11:30:43 -0400

    Support for different IDC methods
    No concurrent rate support yet

--- pages/SCR0020.py
+++ pages/SCR0020.py
@@ -1,22 +1,22 @@
 from pages.Page import Page
-from pages.elements import Text, Select
+from pages.elements import Text, Select, Radio, Row
 
 
 class SCR0020(Page):
     """
     SCR_0020 Indirect cost entry
-    {note}
-    Currently only handles default 1 rate for full project
-    {note}
     """
     locators = {
-        "rate": "css=input[name$='__rateValue']",
-        "type": "css=select[name$='__idcTypeId']",
-        "ok": "name=SelectIndirectCostRateOKEvent"
+        "ok": "name=SelectIndirectCostRateOKEvent",
+        "policy": "css=input[name=rateType]",
+        "rate_row": "xpath=//input[contains(@name,'RateList__')][@class='yearinput']/ancestor::tr[1]"
     }
 
-    rate = Text("rate", "IDC Rate edit box")
-    rate_type = Select("type", "Rate type (dropdown)")
+    rate_policy = Radio("policy", "IDC Rate Policy", mapping={
+        "single": "SingleRatePolicy",
+        "fiscal": "RateByFiscalYearPolicy",
+        "budget": "RateByBudgetPeriodPolicy"
+        })
 
     def ok(self):
         """
@@ -24,3 +24,27 @@ class SCR0020(Page):
         Goes to SCR_0018
         """
         return self.go("ok")
+
+    @property
+    def rate_count(self):
+        """
+        Number of rates
+        """
+        return len(self.finds("rate_row"))
+
+    def rate(self, n):
+        """
+        Returns the nth rate row from the page
+        //Rate
+        """
+        row = self.finds("rate_row")[n - 1]
+        return self.Rate(row, self)
+
+    class Rate(Row):
+        locators = {
+            "rate": "css=input[type=text][name*=RateList__]",
+            "type": "css=select[name$='__idcTypeId']"
+        }
+
+        rate = Text("rate", "IDC Rate edit box")
+        rate_type = Select("type", "Rate type (dropdown)")
