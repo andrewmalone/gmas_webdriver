5268ca567989931f92bceab1c26c7b417ac4e831
andrewmalone <andrew@andrewmalone.com>
2014-05-30 11:31:19 -0400

    Paging through periods, subs, and people

--- pages/SCR0006.py
+++ pages/SCR0006.py
@@ -1,5 +1,5 @@
 from pages.Page import Page
-from pages.elements import Text
+from pages.elements import Text, Row, Radio
 
 
 class SCR0006(Page):
@@ -13,11 +13,21 @@ class SCR0006(Page):
         "editbox": "css=input[name$='__budgetEntryAmount']",
         "next period": "css=input[title='Next period']",
         "ok": "name=EditProposedBudgetOKEvent",
-        "cancel": "EditProposedBudgetCancelEvent"
+        "cancel": "EditProposedBudgetCancelEvent",
+        "subagreement_row": "xpath=//a[contains(@onclick,'setSubagreementIdAndSubmitForm')]/ancestor::tr[1]",
+        "periods": "xpath=//td[@class='strong'][contains(text(),'Period')]",
+        "level": "css=input[name=recordByBudgetLevel]",
+        "category_row": "xpath=//input[@type='text'][contains(@name,'segmentCategoryBudgetEntryAmount')]/ancestor::tr[1]",
+        "person_salary_row": "xpath=//a[contains(@href,'setSCR252fields')/ancestor::tr[1]"
     }
 
     total_direct = Text("total_direct", "Total direct cost")
     total_indirect = Text("total_indirect", "Total indirect cost")
+    budget_level = Radio("level", "Budget level", mapping={
+        "total": "9776",
+        "category": "9777",
+        "line item": "9778"
+        })
 
     def enter_first(self, amount):
         """
@@ -29,6 +39,20 @@ class SCR0006(Page):
             if elem.is_displayed() is True:
                 elem.send_keys(amount)
                 break
+
+    @property
+    def period_count(self):
+        """
+        Total number of budget periods
+        """
+        return self.find("periods").text[12]
+
+    @property
+    def current_period(self):
+        """
+        Current budget period
+        """
+        return self.find("periods").text[7]
 
     def next(self):
         """
@@ -57,3 +81,79 @@ class SCR0006(Page):
         """
         return self.go("cancel")
 
+    @property
+    def subagreement_count(self):
+        """
+        Number of subagreements in the budget
+        """
+        return len(self.finds("subagreement_row"))
+
+    def subagreement(self, n):
+        """
+        Returns the nth subagreement row in the budget
+        //Subagreement
+        """
+        element = self.finds("subagreement_row")[n - 1]
+        return self.Subagreement(element, self)
+
+    class Subagreement(Row):
+        locators = {
+            "link": "css=a"
+        }
+
+        def go(self):
+            """
+            Click the subagreement link
+            Goes to SCR_00017
+            """
+            return self._go("link")
+
+    @property
+    def category_count(self):
+        """
+        Number of categories
+        """
+        return len(self.finds("category_row"))
+
+    def category(self, n):
+        """
+        Returns the nth category row
+        //Category
+        """
+        row = self.finds("category_row")[n - 1]
+        return self.Category(row, self)
+
+    class Category(Row):
+        locators = {
+            "input": "css=input[type=text]"
+        }
+
+        amount = Text("input", "Category amount entry")
+
+    @property
+    def person_count(self):
+        """
+        Count of people
+        """
+        # dividing by two here because each person appears twice for salary and fringe
+        return len(self.finds("person_salary_row")) / 2
+
+    def person(self, n):
+        """
+        Returns the nth person
+        //Person_salary
+        """
+        row = self.finds("person_salary_row")[n - 1]
+        return self.Person_salary(row, self)
+
+    class Person_salary(Row):
+        locators = {
+            "link": "css=a"
+        }
+
+        def go(self):
+            """
+            Click the person link
+            Goes to SCR_0252
+            """
+            return self._go("link")
