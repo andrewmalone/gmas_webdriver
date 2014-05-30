ed7b3dfedc2185c57e747ff78aaa19b548b3f5b7
andrewmalone <andrew@andrewmalone.com>
2014-05-30 11:30:15 -0400

    Support for paging through budget periods and subs

--- pages/SCR0031.py
+++ pages/SCR0031.py
@@ -1,4 +1,6 @@
 from pages.Page import Page
+from pages.elements import Row
+import utilities.xpath as xpath
 
 
 class SCR0031(Page):
@@ -8,8 +10,25 @@ class SCR0031(Page):
     locators = {
         "edit": "ViewProposedBudgetEditPostsubmissionEvent",
         "indirect": "event=ViewProposedBudgetViewIndirectCostRateEvent",
-        "modular": "event=ViewProposedBudgetViewModularBudgetEvent"
+        "modular": "event=ViewProposedBudgetViewModularBudgetEvent",
+        "next period": "css=input[title='Next period']",
+        "periods": "xpath=//td[@class='strong'][contains(text(),'Period')]",
+        "subagreement_row": xpath.parent_row_of_event("ViewProposedBudgetSubagreementEvent")
     }
+
+    @property
+    def period_count(self):
+        """
+        Total number of budget periods
+        """
+        return self.find("periods").text[12]
+
+    @property
+    def current_period(self):
+        """
+        Current budget period
+        """
+        return self.find("periods").text[7]
 
     def edit(self):
         """
@@ -41,3 +60,35 @@ class SCR0031(Page):
         else:
             return True
 
+    def next(self):
+        """
+        Click the next period button
+        """
+        return self.go("next period")
+
+    @property
+    def subagreement_count(self):
+        """
+        Number of subagreements in the budget
+        """
+        return len(self.finds("subagreement_row"))
+
+    def subagreement(self, n):
+        """
+        Returns the nth subagreement row in the budget
+        //Subagreement
+        """
+        element = self.finds("subagreement_row")[n - 1]
+        return self.Subagreement(element, self)
+
+    class Subagreement(Row):
+        locators = {
+            "link": "event=ViewProposedBudgetSubagreementEvent"
+        }
+
+        def go(self):
+            """
+            Click the subagreement link
+            Goes to SCR_00017
+            """
+            return self._go("link")
