85c7b317f8b608f8f7bceb12be5acf2f14e22e16
andrewmalone <andrew@andrewmalone.com>
2014-08-13 16:19:08 -0400

    Set fund type and fund

--- pages/SCR0184.py
+++ pages/SCR0184.py
@@ -1,25 +1,31 @@
 from pages.Page import Page
+from pages.elements import Select, Text
 
-		
+
 class SCR0184(Page):
-	"""
-	SCR_0184 Create Fund
-	"""
-	locators = {
-		"cancel" : "name=CreateFundCancelEvent",
-		"ok": "CreateFundOKEvent"
-	}
-	
-	def cancel(self):
-		"""
-		Click <Cancel>
-		Goes to SCR_0474
-		"""
-		return self.go("cancel")
+    """
+    SCR_0184 Create Fund
+    """
+    locators = {
+        "cancel": "name=CreateFundCancelEvent",
+        "ok": "CreateFundOKEvent",
+        "fund_type": "fundType",
+        "fund": "fundValue"
+    }
 
-	def ok(self):
-		"""
-		Click <Ok>
-		Goes to SCR_0474
-		"""
-		return self.go("ok")	
+    fund_type = Select("fund_type", "Fund type")
+    fund = Text("fund", "Fund value")
+
+    def cancel(self):
+        """
+        Click <Cancel>
+        Goes to SCR_0474
+        """
+        return self.go("cancel")
+
+    def ok(self):
+        """
+        Click <Ok>
+        Goes to SCR_0474
+        """
+        return self.go("ok")
