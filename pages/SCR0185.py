517a4ff22471a301129b23f6c3c06fa16dc451e4
andrewmalone <andrew@andrewmalone.com>
2014-08-15 13:55:38 -0400

    Added activity text box and get button

--- pages/SCR0185.py
+++ pages/SCR0185.py
@@ -1,5 +1,5 @@
 from pages.Page import Page
-from pages.elements import Select
+from pages.elements import Select, Text
 
 a21_map = {
     "A01": "A01-Instruction & Training",
@@ -8,6 +8,7 @@ a21_map = {
     "A15": "A15-Scholarships & Student Aid"
 }
 
+
 class SCR0185(Page):
     """
     SCR_0185 Create new activity
@@ -15,10 +16,18 @@ class SCR0185(Page):
     locators = {
         "a21": "a21FunctionalCode",
         "ok": "CreateActivityOKEvent",
-        "get": "GetNewActivityForCreateEvent"
+        "get": "GetNewActivityForCreateEvent",
+        "activity": "activityValue"
     }
 
     a21 = Select("a21", "A-21 code", a21_map)
+    activity = Text("activity", "Activity value")
+
+    def get_activity(self):
+        """
+        Clicks <Get new activity>
+        """
+        return self.go("get")
 
     def ok(self):
         """
