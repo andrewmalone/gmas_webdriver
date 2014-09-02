4b40f144a0ec4324a249dd9a07e27f562f655bc3
andrewmalone <andrew@andrewmalone.com>
2014-09-02 16:53:26 -0400

    Returns the start/end dates if end - start is less than 1 year

--- utilities/dates.py
+++ utilities/dates.py
@@ -13,12 +13,15 @@ def budget_period_dates(start, end, fmt="%m-%d-%Y"):
     """
     For any project start/end dates, returns a list of tuples representing the dates of each budget period.
     Assumes year long budget periods
+    Returns the start/end dates if end - start is less than 1 year
     """
     sd = datetime.strptime(start, fmt)
     ed = datetime.strptime(end, fmt)
-    diff = ed - sd
+    diff = (ed + timedelta(days=1)) - sd
     # http://stackoverflow.com/questions/4436957/pythonic-difference-between-two-dates-in-years
     diff_years = (diff.days + diff.seconds/86400.0)/365.2425
+    if diff_years < .95:
+        return (start, end)
     periods = int(round(diff_years))
     period_dates = []
     for i in range(periods):
@@ -26,3 +29,7 @@ def budget_period_dates(start, end, fmt="%m-%d-%Y"):
         end = datetime.strftime(datetime(sd.year + (i + 1), sd.month, sd.day) - timedelta(days=1), fmt)
         period_dates.append((start, end))
     return period_dates
+
+
+if __name__ == "__main__":
+    pass
