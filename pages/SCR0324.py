4a817927b17740ad6edc82680e6f520b3bd2c94c
andrewmalone <andrew@andrewmalone.com>
2014-09-03 09:59:34 -0400

    PEP8

--- pages/SCR0324.py
+++ pages/SCR0324.py
@@ -4,34 +4,34 @@ from pages.lookups import Lookup_organization, Lookup_person
 
 
 class SCR0324(Page):
-	"""
-	SCR_0324 Edit sponsor information
-	"""
-	locators = {
-		"sponsor" : "name=sponsorName",
-		"prime sponsor" : "name=primeSponsorName",
-		"ok" : "name=EditSponsorInfoDoneEvent",
-		"next": "name=EditSponsorInfoNextEvent",
-		"prime pi" : "name=primePiName"
-	}
+    """
+    SCR_0324 Edit sponsor information
+    """
+    locators = {
+        "sponsor": "name=sponsorName",
+        "prime sponsor": "name=primeSponsorName",
+        "ok": "name=EditSponsorInfoDoneEvent",
+        "next": "name=EditSponsorInfoNextEvent",
+        "prime pi": "name=primePiName"
+    }
 
-	sponsor_text = Text("sponsor", "Sponsor")
-	sponsor = Lookup_organization(sponsor_text, "sponsorLookupImage", "Sponsor")
-	prime_sponsor_text = Text("prime sponsor", "Prime sponsor")
-	prime_sponsor = Lookup_organization(prime_sponsor_text, "primeSponsorLookupImage", "Prime sponsor")
-	prime_pi_text = Text("prime pi", "Prime PI")
-	prime_pi = Lookup_person(prime_pi_text, "primePiImage", "Prime PI")
-		
-	def ok(self):
-		"""
-		Click <Done making revisions to this section>
-		Goes to SCR_0105
-		"""
-		return self.go("ok")
+    sponsor_text = Text("sponsor", "Sponsor")
+    sponsor = Lookup_organization(sponsor_text, "sponsorLookupImage", "Sponsor")
+    prime_sponsor_text = Text("prime sponsor", "Prime sponsor")
+    prime_sponsor = Lookup_organization(prime_sponsor_text, "primeSponsorLookupImage", "Prime sponsor")
+    prime_pi_text = Text("prime pi", "Prime PI")
+    prime_pi = Lookup_person(prime_pi_text, "primePiImage", "Prime PI")
 
-	def next(self):
-		"""
-		Click <Next>
-		Goes to SCR_0403b
-		"""
-		return self.go("next")
+    def ok(self):
+        """
+        Click <Done making revisions to this section>
+        Goes to SCR_0105
+        """
+        return self.go("ok")
+
+    def next(self):
+        """
+        Click <Next>
+        Goes to SCR_0403b
+        """
+        return self.go("next")
