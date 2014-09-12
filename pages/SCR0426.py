24288de43daaa8ef1c164d673c8419bcb5429753
andrewmalone <andrew@andrewmalone.com>
2014-09-12 11:44:48 -0400

    Add open all link

--- pages/SCR0426.py
+++ pages/SCR0426.py
@@ -6,5 +6,11 @@ class SCR0426(Page):
     SCR_0426 Segment revision detail
     """
     locators = {
-        
+        "open_all": "link=open all"
     }
+
+    def open_all(self):
+        """
+        Click "open all"
+        """
+        return self.go("open_all")
