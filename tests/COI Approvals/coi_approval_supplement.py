288b04f4dde016d55f2f5009a687ed4768fd631d
andrewmalone <andrew@andrewmalone.com>
2014-10-03 14:19:46 -0400

    Supplement tests

--- tests/COI Approvals/coi_approval_supplement.py
+++ tests/COI Approvals/coi_approval_supplement.py
@@ -163,14 +163,39 @@ def add_person(huid, key="true", investigator="true"):
     people[huid] = {
         "investigator": investigator,
         "OAR": 0,
-        "FCOI": 0
+        "FCOI": 0,
+        "added": True
     }
 
 
-class test_coi_continuation(unittest.TestCase):
+def add_person_supplement(huid, key="true", investigator="true"):
+    global supplement_data, people
+    supplement_data["research team"].append({
+        "huid": huid,
+        "role": "Consultant",
+        "key": key,
+        "investigator": investigator,
+        "hs": "false"
+        })
+    people[huid] = {
+        "investigator": investigator,
+        "OAR": 0,
+        "FCOI": 0,
+        "added": True
+    }
+
+
+class test_coi_supplement(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        # create an awarded project...
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        global p
+        # p.quit()
+
+    def setUp(self):
         global p
         r = request()
         init_people(r)
@@ -196,17 +221,8 @@ class test_coi_continuation(unittest.TestCase):
         self.segment = p.get_id("segment")
         print "Created segment %s" % self.segment
 
-    @classmethod
-    def tearDownClass(self):
-        global p
-        # p.quit()
-
-    def setUp(self):
-        global p
         if p.scr != "SCR0104":
             p = p.goto_segment(self.segment)
-        p = supplement(p, supplement_data)
-        init_people(self.request)
 
     def tearDown(self):
         pass
@@ -215,6 +231,21 @@ class test_coi_continuation(unittest.TestCase):
         self.get_approval_counts()
         for person in people:
             self.assertEqual(people[person]["OAR"], 1 if people[person]["investigator"] == "true" else 0)
+            self.assertEqual(people[person]["FCOI"], 0)
+
+    def assertOARSegment(self):
+        global p
+        if p.scr != "SCR0104":
+            p = p.project_snapshot.goto_segment_home()
+        self.get_approval_counts()
+        for person in people:
+            expected_oar = 0
+            if people[person]["investigator"] == "true":
+                if "added" in people[person] and people[person]["added"] is True:
+                    expected_oar = 1
+                else:
+                    expected_oar = 2
+            self.assertEqual(people[person]["OAR"], expected_oar)
             self.assertEqual(people[person]["FCOI"], 0)
 
     def assertNone(self):
@@ -237,19 +268,53 @@ class test_coi_continuation(unittest.TestCase):
                 people[approval.huid]["OAR"] += 1
         p = p.back()
 
-    def test_01_supplement_basic(self):
+    def test_supplement_basic(self):
         global p
+        p = supplement(p, supplement_data)
         self.assertOAR()
+        self.assertOARSegment()
 
-    def _test_02_add_people(self):
+    def test_add_people(self):
         global p
+        p = supplement(p, supplement_data)
         add_person(data.random_huid()[0], key="true")
         add_person(data.random_huid()[0], key="false", investigator="false")
         add_person(data.random_huid()[0], key="false", investigator="true")
-        self.assertNone()
+        self.assertOAR()
+        self.assertOARSegment()
 
-    def _test_03_award_supplement(self):
+    def test_add_people_rgs(self):
         global p
+        add_person_supplement(data.random_huid()[0], key="true")
+        add_person_supplement(data.random_huid()[0], key="false", investigator="false")
+        add_person_supplement(data.random_huid()[0], key="false", investigator="true")
+        p = supplement(p, supplement_data)
+        self.assertOAR()
+        self.assertOARSegment()
+
+    def test_change_flags(self):
+        global p
+        p = supplement(p, supplement_data)
+        p = p.goto_research_team()
+        p = p.goto_role(4)
+        p = p.edit_personnel()
+        p.set_key("true")
+        p.appt = "12"
+        self.request["research team"][2]["key"] = "true"
+        self.request["research team"][2]["investigator"] = "true"
+        p = p.ok().back()
+        p = p.goto_role(5)
+        p = p.edit_personnel()
+        p.phs = "true"
+        p.appt = "12"
+        self.request["research team"][3]["investigator"] = "true"
+        init_people(self.request)
+        p = p.ok().back(2)
+        self.assertOAR()
+
+    def test_award_supplement(self):
+        global p
+        p = supplement(p, supplement_data)
         add_person(data.random_huid()[0], key="true")
         add_person(data.random_huid()[0], key="false", investigator="false")
         add_person(data.random_huid()[0], key="false", investigator="true")
@@ -259,7 +324,7 @@ class test_coi_continuation(unittest.TestCase):
         p = p.review_completed()
         p = awarding_revision(p, {}, minimal=True, notification=False)
         p = p.back(2)
-        self.assertOAR()
+        self.assertOARSegment()
 
 if __name__ == '__main__':
     sponsor = "nih"
