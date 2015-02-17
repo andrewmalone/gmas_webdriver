671ff0a30347ad55752a1e23b941c3e3dc4e57f4
andrewmalone <andrew@andrewmalone.com>
2015-02-17 14:46:00 -0500

    Add ability to change org on SCR_0400

--- pages/SCR0400.py
+++ pages/SCR0400.py
@@ -1,6 +1,6 @@
 from pages.Page import Page
 from pages.elements import Text
-from pages.lookups import Lookup_person
+from pages.lookups import Lookup_person, Lookup_org
 
 
 class SCR0400(Page):
@@ -9,11 +9,14 @@ class SCR0400(Page):
     """
     locators = {
         "pi_text": "piName",
+        "org_text": "org",
         "ok": "EditInitialRequestInfoOKEvent"
     }
 
     pi_text = Text("pi_text", "Edit box for PI name")
     pi = Lookup_person(pi_text, "piImage", "PI Lookup")
+    org_text = Text("org_text", "Edit box for org")
+    org = Lookup_org(org_text, "picture", "Org lookup")
 
     def ok(self):
         """
