ceb3db8332545cadb91c9d8872f8ff2e5b910e6c
andrewmalone <andrew@andrewmalone.com>
2015-02-20 15:44:10 -0500

    Add tests for removing members and changing flags

--- tests/COI Approvals/test_coi_approvals_confirm.py
+++ tests/COI Approvals/test_coi_approvals_confirm.py
@@ -16,7 +16,7 @@ class Test_COI_Confirm(common.COI_Test):
         self.confirm_team()
         self.assert_approvals()
 
-    @pytest.mark.dev
+    @pytest.mark.combined
     def test_confirm_add_team(self):
         """
         Add standard team at confirm
@@ -27,9 +27,26 @@ class Test_COI_Confirm(common.COI_Test):
         self.confirm_team()
         self.assert_approvals()
 
+    @pytest.mark.combined
+    def test_confirm_remove_team(self):
+        title = "COI confirm team delete people"
+        self.add_standard_team()
+        self.create_to_confirm(title, submit_oar=True)
+        self.delete_team_at_confirm()
+        self.confirm_team()
+        self.assert_approvals()
+
+    @pytest.mark.dev
+    def test_confirm_change_flags(self):
+        title = "COI confirm team change flags"
+        self.add_standard_team()
+        self.create_to_confirm(title, submit_oar=True)
+        self.change_confirm_flags()
+        self.confirm_team()
+        self.assert_approvals()
 
 if __name__ == "__main__":
     # pytest.main(['-m combined', '-v', ''])
     import os
     filename = os.path.basename(__file__)
-    pytest.main(['%s' % filename, '-m dev', ''])
+    pytest.main(['%s' % filename, '-m dev', '-s'])
