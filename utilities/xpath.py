2678e459fca272dd06845785e768f871002189c9
andrewmalone <andrew@andrewmalone.com>
2015-03-28 11:35:25 -0400

    Fix PEP8 whitespace

--- utilities/xpath.py
+++ utilities/xpath.py
@@ -1,5 +1,6 @@
 def text_sibling(element, text, num):
     return "xpath=//%s[contains(normalize-space(text()), '%s')]/following-sibling::%s[%s]" % (element, text, element, num)
 
+
 def parent_row_of_event(event):
     return "xpath=//a[contains(@href,'%s')]/ancestor::tr[1]" % event
