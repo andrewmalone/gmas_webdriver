a94a2f30d3bfdf2001e01500ddbef93068840677
Anusharao <anushabellamkonda06@gmail.com>
2015-04-22 10:20:04 -0400

    SCR0511

--- pages/SCR0511.py
+++ pages/SCR0511.py
@@ -10,12 +10,24 @@ class SCR0511(Page):
     locators = {
             "disable": "DisableSubactivityEvent",
             "reenable": "ReenableSubactivityEvent",
-            "GL_upload_history": "xpath=//td[contains(text(), 'General ledger upload history')]/ancestor::table[1]//tr[@class='bg0'][position()>1]",
+            "GL_upload_history": "xpath=//td[contains(text(), 'General ledger upload history')]/ancestor::table[1]//tr[@class='bg0'][position()>2]",
             "gl status": xpath.text_sibling("td", "General ledger status", 2),
             "owning tub": xpath.text_sibling("td", "Owning tub", 2),
             "description": xpath.text_sibling("td", "Description", 2),
             "year": xpath.text_sibling("td", "Year", 2)
     }
+
+    _locators = {
+            "GL_upload_history": "css=[id$=GLUploadHistory_content] tbody tr",
+            "gl status": "css=[id$=glStatus]",
+            "owning tub": "css=[id$=tub]",
+            "description": "css=[id$=accountDescription]",
+            "year": "css=[id$=year]",
+            "descriptor": "css=[id$=Descriptor]"
+    }
+
+
+
 
     @classmethod
     def url(cls, account_id, segment_id, subactivity_id, fund):
@@ -24,7 +36,7 @@ class SCR0511(Page):
         """
         url = "{{}}/gmas/dispatch?ref=%2Faccount%2FSCR0187AccountDetail.jsp&accountId={}&segmentId={}&formName=AccountDetailForm&subactivityId={}&fund={}&ViewSubactivityDetailsEvent=&submit"
         return url.format(account_id, segment_id, subactivity_id, fund)
-    
+  
     gl_status = RText("gl status", "General ledger status")
     owning_tub = RText("owning tub", "Owning tub")
     description = RText("description", "Description")
@@ -57,6 +69,12 @@ class SCR0511(Page):
             "date": Row.cell(2),
             "type": Row.cell(6),
             "feed_status": Row.cell(10)
+        }
+        
+        _locators = {
+            "date": Row.cell(1),
+            "type": Row.cell(2),
+            "feed_status": Row.cell(3)       
         }
         date = RText("date", "GL date")
         type = RText("type", "GL action type")
