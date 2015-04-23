b43008c01d36ad05a28b3b7816452c6185aebcb8
Anusharao <anushabellamkonda06@gmail.com>
2015-04-23 14:21:06 -0400

    SCR308

--- pages/SCR0308.py
+++ pages/SCR0308.py
@@ -1,5 +1,5 @@
 from pages.Page import Page
-from pages.elements import Row
+from pages.elements import Row, RText
 import utilities.xpath as xpath
 
 
@@ -12,6 +12,14 @@ class SCR0308(Page):
         "notice link": "css=a[href*='ViewDetailsOfNoticeEvent']",
         "notice row": xpath.parent_row_of_event("ViewDetailsOfNoticeEvent")
     }
+    
+    @classmethod
+    def url(cls, segment_id):
+        """
+        Direct navigation to SCR_0308
+        """
+        url = "{{}}/gmas/dispatch?ref=%2Fnotice%2FCOM0873NoticeSummary.jsp&ViewNoticeListEvent=&segmentId={}&formName=SegmentHomeForm&submit"
+        return url.format(segment_id)
 
     @property
     def notice_count(self):
@@ -41,10 +49,24 @@ class SCR0308(Page):
         """
         row = self.finds("notice row")[n - 1]
         return self.Notice_row(row, self)
-
+    
+    @property
+    def notices(self):
+        """
+        Returns the nth notice in the list
+        //Notice_row
+        """
+        return [self.Notice_row(row, self) for row in self.finds("notice row")]
+    
     class Notice_row(Row):
         locators = {
-            "link": "event=ViewDetailsOfNoticeEvent"
+            "link": "event=ViewDetailsOfNoticeEvent",
+            "title": Row.cell(3),
+            "sponsor award no": Row.cell(7),
+            "amendment": Row.cell(11),
+            "date received": Row.cell(15),
+            "status": Row.cell(19),
+            "type": Row.cell(23)
         }
 
         def go(self):
@@ -53,3 +75,11 @@ class SCR0308(Page):
             Goes to SCR_0309
             """
             return self._go("link")
+        title = RText("title", "Title")
+        sponsor_award_no = RText("sponsor award no", "Sponsor award no")
+        amendment = RText("amendment", "Amendment/modification no.")
+        date_received = RText("date received", "Date received by Harvard")
+        status = RText("status", "Status")
+        type = RText("type", "Type")
+
+        
