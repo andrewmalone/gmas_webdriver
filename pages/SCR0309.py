a8a077692b1c3dd317f7783521cf177846952e9f
Anusharao <anushabellamkonda06@gmail.com>
2015-04-24 16:18:31 -0400

    SCR0309

--- pages/SCR0309.py
+++ pages/SCR0309.py
@@ -1,5 +1,6 @@
 from pages.Page import Page
-
+from pages.elements import Row, RText
+import utilities.xpath as xpath
 
 class SCR0309(Page):
     """
@@ -10,8 +11,35 @@ class SCR0309(Page):
         "revise project": "CreateNewRevisionEvent",
         "documents": "link=Documents",
         "status": "xpath=//td[contains(text(), 'Notice status')]",
-        "edit_attributes": "EditNoticeAttributesEvent"
+        "edit_attributes": "EditNoticeAttributesEvent",
+        "notice title": xpath.text_sibling("td", "Notice Title", 2),
+        "sponsor award no": xpath.text_sibling("td", "Sponsor award no.", 2),
+        "amendment no": xpath.text_sibling("td", "Amendment no.", 2),
+        "form of notice": xpath.text_sibling("td", "Form of notice", 2),
+#         "date issued": xpath.text_sibling("td", "Date issued", 2),
+        "date issued": "xpath=//span[contains(normalize-space(text()), 'Date issued')]/../following-sibling::td[2]",
+        "date received": "xpath=//span[contains(normalize-space(text()), 'Date received by Harvard')]/../following-sibling::td[2]",
+        "date fully executed": "xpath=//span[contains(normalize-space(text()), 'Date fully executed')]/../following-sibling::td[2]",
+        "request row": "xpath=//a[contains(@href,'RequestHomeEvent')]/ancestor::tr[1]",
+        "document_count": "xpath=//*[contains(normalize-space(text()), 'Documents')]/ancestor::td[1]/following-sibling::td[2]"
     }
+    
+    @classmethod
+    def url(cls, notice_id, segment_id):
+        """
+        Direct navigation to SCR_0309
+        """
+        url = "{{}}/gmas/dispatch?ref=%2Fnotice%2FSCR0308ListofNoticesInASegment.jsp&noticeId={}&segmentId={}&formName=ListOfNoticesForm&ViewDetailsOfNoticeEvent=&submit"
+        return url.format(notice_id, segment_id)
+    
+    notice_title = RText("notice title", "Notice Title")
+    sponsor_awardno = RText("sponsor award no", "Sponsored award no")
+    amendment_no = RText("amendment no", "Amendment no.")
+    formof_notice = RText("form of notice", "Form of notice")
+    date_issued = RText("date issued", "Date issued")
+    date_received = RText("date received", "Date received by harvard")
+    date_fullyexecuted = RText("date fully executed", "Date fully executed")
+    document_count = RText("document_count", "Documents count")
 
     @property
     def notice_status(self):
@@ -47,3 +75,46 @@ class SCR0309(Page):
         Goes to SCR_0453
         """
         return self.go("edit_attributes")
+    
+    def request(self, n):
+        """
+        Returns the nth request in the list
+        //Request_row
+        """
+        row = self.finds("request row")[n - 1]
+        return self.Request_row(row, self) 
+    
+    @property
+    def requests(self):
+        """
+        Returns the nth request in the list
+        //Reuest_row
+        """
+        return [self.Request_row(row, self) for row in self.finds("request row")]
+    
+    class Request_row(Row):
+        locators = {
+            "link": "event = RequestHomeEvent",
+            "request title": Row.cell(2),
+            "request type": Row.cell(6),
+            "proposed dates": Row.cell(10),
+            "proposed dollars": Row.cell(14),
+            "submitted": Row.cell(18)
+        }
+        
+        def go(self):
+            """
+            Click the request link
+            Goes to SCR_0115
+            """
+            return self._go("link")
+        request_title = RText("request title", "Request title")
+        request_type = RText("request type", "Request type")
+        proposed_dates = RText("proposed dates", "Proposed dates")
+        Proposed_dollars = RText("proposed dollars", "Proposed dollars")
+        submitted = RText("submitted", "Submitted")
+        
+        
+        
+
+
