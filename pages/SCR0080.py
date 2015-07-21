1a7527a42d6f956a107bc2e6e587367dd1fb37b7
Anusharao <anushabellamkonda06@gmail.com>
2015-07-21 16:37:40 -0400

    scr0080

--- pages/SCR0080.py
+++ pages/SCR0080.py
@@ -1,5 +1,6 @@
 from pages.Page import Page
 from pages.elements import Row
+from pages.elements import RText
 import utilities.xpath as xpath
 import utilities.url as url
 
@@ -12,6 +13,15 @@ class SCR0080(Page):
         "approval link": "event=ApprovalListViewOrEditDetailEvent",
         "approval row": xpath.parent_row_of_event("ApprovalListViewOrEditDetailEvent")
     }
+
+
+    @classmethod
+    def url(cls, segment_id):
+        """
+        Direct navigation to SCR_0015
+        """
+        url = "{{}}/gmas/dispatch?ref=%2Fproject%2Fincludes%2Fsegmenthome%2FSegmentHomeBody.jsp&segmentId={}&ApprovalListHomeEvent=&formName=SegmentHomeForm"
+        return url.format(segment_id)
 
     @property
     def approval_count(self):
@@ -47,8 +57,25 @@ class SCR0080(Page):
 
     class Approval_row(Row):
         locators = {
-            "link": "event=ApprovalListViewOrEditDetailEvent"
+            "link": "event=ApprovalListViewOrEditDetailEvent",
+            "status": Row.cell(3),
+            "approval type": Row.cell(7),
+            "resp. party": Row.cell(11),
+            "due date": Row.cell(15),
+            "effective": Row.cell(19),
+            "expires": Row.cell(23),
+            "m": Row.cell(27),
+            "c": Row.cell(31)
         }
+        
+        status = RText("status", "Status")
+        approval_type = RText("approval type", "Approval type")
+        resp_party = RText("resp. party", "Resp. party")
+        due_date = RText("due date", "Due date")
+        effective = RText("effective", "Effective")
+        expires = RText("expires", "Expires")
+        m = RText("m", "M")
+        c = RText("c", "C")
 
         @property
         def type(self):
@@ -86,3 +113,5 @@ class SCR0080(Page):
             Goes to SCR_0081
             """
             return self._go("link")
+        
+    
