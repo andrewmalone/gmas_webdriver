6e9a0a72fb612b00c516addd4897806fade923c2
Anusharao <anushabellamkonda06@gmail.com>
2016-01-21 16:23:45 -0500

    SCR_0300

--- pages/SCR0300.py
+++ pages/SCR0300.py
@@ -13,17 +13,40 @@ class SCR0300(Page):
         "row by role": "xpath=//*[contains(text(),'REPLACE')][not(@class)]/ancestor::tr[1]",
         "row": xpath.parent_row_of_event("ProjectAdminTeamEditRolesEvent"),
         "add": "ProjectAdminTeamAddRolesEvent",
-        "administrative team": "xpath=//*[contains(@href, 'Role')]/ancestor::table[1]//tr[not (@class ='bg3')][position()>2]"
+        "administrative team": "xpath=//*[contains(@href, 'Role')]/ancestor::table[1]//tr[not (@class ='bg3')][position()>2]",
+        "more details": "link=More details...",
+        "admin team": "link=Administrative team"
         
+    }
+    
+    _locators = {
+        "role": "link=REPLACE",
+        "row by person": "xpath=//a[contains(@href,'ProjectAdminTeamPersonProfileEvent')][contains(@href,'personId=REPLACE')]/ancestor::tr[1]",
+        "row by role": "xpath=//*[contains(text(),'REPLACE')][not(@class)]/ancestor::tr[1]",
+        "row": xpath.parent_row_of_event("ProjectAdminTeamEditRolesEvent"),
+        "add": "ProjectAdminTeamAddRolesEvent",
+        "administrative team": "xpath=//*[(@id ='j_idt125:projectAdministrativeTeamDatatable_data')]//tr[not (@class ='bg3')][position()>0]",
+        "more details": "link=More details...",
+        "admin team": "link=Administrative team"          
+                 
     }
 
     @classmethod
-    def url(cls, segmentId):
+    def url(cls, segment_id, project_id):
         """
         Direct navigation to SCR_0300
         """
-        url = "{{}}/gmas/dispatch?segmentId={}&formName=SegmentHomeForm&SegmentSummaryAdministrativeTeamLinkEvent="
-        return url.format(segmentId)
+        url = "{{}}/gmas/dispatch?ref=%2Fuser%2FSCR0270GMASHomePage.jsp&segmentId={}&formName=SegmentHomeForm&projectId={}&ProjectListSegmentHomeEvent"
+        return url.format(segment_id, project_id)
+
+#     @classmethod
+#     def url(cls, segment_id, project_id):
+#         """
+#         Direct navigation to segmenthome
+#         """
+#         url = "{{}}/gmas/dispatch?ref=%2Fuser%2FSCR0270GMASHomePage.jsp&segmentId={}&formName=SegmentHomeForm&projectId={}&ProjectListSegmentHomeEvent"
+#         return url.format(segment_id, project_id)
+
  
     
     @property
@@ -79,6 +102,17 @@ class SCR0300(Page):
         Goes to SCR_0416
         """
         return self.go("add")
+    
+    def segment_home(self):
+        try:
+            return self.go("more details")
+        except:
+            self.find("open all").click()
+            return self
+    
+    
+    def admin_team(self):
+        return self.go("admin team")
 #     
     @property
     def row(self):
@@ -90,11 +124,13 @@ class SCR0300(Page):
     class Row(Row):
         locators = {
             "role":  Row.cell(3),
-            "name":  Row.cell(7),
-            "role link": "css=a[href*=ProjectAdminTeamEditRolesEvent]",
-            "name link": "css=a[href*=ProjectAdminTeamPersonProfileEvent]"
+            "name":  Row.cell(7)
         }
         
+        _locators = {
+            "role":  Row.cell(1),
+            "name":  Row.cell(2)
+            }
         role = RText("role", "Role name")
         name = RText("name", "Person name")
 
