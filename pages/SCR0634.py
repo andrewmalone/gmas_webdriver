4e808862f018080b599dadaa8bc962d76489f906
andrewmalone <andrew@andrewmalone.com>
2016-03-21 08:55:36 -0400

    Extending 634 implementation

--- pages/SCR0634.py
+++ pages/SCR0634.py
@@ -6,8 +6,17 @@ class SCR0634(Page):
     SCR_0634 Segment documents
     """
     locators = {
-        "document": "event=DocumentActionViewEvent"
+        "document": "event=DocumentActionViewEvent",
+        "email": "event=EmailLinkEvent"
     }
+
+    @classmethod
+    def url(cls, segment_id):
+        """
+        Direct navigation to SCR_0634
+        """
+        url = "{{}}/gmas/dispatch?ref=/project/includes/segmenthome/RightColumn.jsp&formName=SegmentHomeForm&segmentId={}&SegmentDocumentsLinkEvent="
+        return url.format(segment_id)
 
     @property
     def document_count(self):
@@ -15,3 +24,10 @@ class SCR0634(Page):
         Number of documents shown
         """
         return len(self.finds("document"))
+
+    @property
+    def email_count(self):
+        """
+        Number of emails shown
+        """
+        return len(self.finds("email"))
