b385a3a77c947b4cf417db017d34b71066c8529f
andrewmalone <andrew@andrewmalone.com>
2016-03-21 08:55:53 -0400

    Added question support for FORMS-D forms

--- pages/SCR0612b.py
+++ pages/SCR0612b.py
@@ -92,21 +92,45 @@ class SCR0612b(Page):
     ps_country = Q_Select(3300719,"Performance site country (dropdown)")
     ps_district = Q_Text(3300720, "Performance site congressional district (text)")
 
-    phs_cover_1a1 = Q_Radio(3301625, "Clinical trial (radio)")
-    phs_cover_1a2 = Q_Radio(3301628, "Phase III Clinical trial (radio)")
-    phs_cover_2 = Q_Radio(3301663, "Disclosure permission (radio)")
-    phs_cover_cell_line = Q_Text(3301640, "Cell line (text)")
+    ###
+    # Cover page supplement 3.0
+    ###
+    phs_cover_1a1 = Q_Radio(3300302, "Clinical trial (radio)")
+    phs_cover_1a2 = Q_Radio(3300305, "Phase III Clinical trial (radio)")
+    phs_cover_2a1 = Q_Radio(3300309, "Vertebrate animals euthanized (radio)")
+    phs_cover_2a2 = Q_Radio(3300312, "AVMA guidelines (radio)")
+    phs_cover_2a3 = Q_Text(3300315, "Scientific justification (text)")
+    phs_cover_cell_line = Q_Text(3300320, "Cell line (text)")
     # TODO - add checkbox!
-    phs_cover_4 = Q_Radio(3301668, "Inventions and Patents (radio)")
-    phs_cover_4a = Q_Radio(3301670, "Inventions previously reported (radio)")
-    phs_cover_5 = Q_Radio(3301648, "Change of investigator (radio)")
-    phs_cover_5a_prefix = Q_Select(3301650, "Prefix (dropdown)")
-    phs_cover_5a_firstname = Q_Text(3301651, "First name (text)")
-    phs_cover_5a_middlename = Q_Text(3301652, "Middle name (text)")
-    phs_cover_5a_lastname = Q_Text(3301653, "Last name (text)")
-    phs_cover_5a_suffix = Q_Select(3301654, "Suffix (dropdown)")
-    phs_cover_6 = Q_Radio(3301655, "Change of institution (radio)")
-    phs_cover_6a = Q_Text(3301656, "Institution name (text)")
+    phs_cover_4 = Q_Radio(3300322, "Inventions and Patents (radio)")
+    phs_cover_4a = Q_Radio(3300325, "Inventions previously reported (radio)")
+    phs_cover_5 = Q_Radio(3300328, "Change of investigator (radio)")
+    phs_cover_5a_prefix = Q_Select(3300331, "Prefix (dropdown)")
+    phs_cover_5a_firstname = Q_Text(3300332, "First name (text)")
+    phs_cover_5a_middlename = Q_Text(3300333, "Middle name (text)")
+    phs_cover_5a_lastname = Q_Text(3300334, "Last name (text)")
+    phs_cover_5a_suffix = Q_Select(3300335, "Suffix (dropdown)")
+    phs_cover_6 = Q_Radio(3300337, "Change of institution (radio)")
+    phs_cover_6a = Q_Text(3300339, "Institution name (text)")
+
+    ###
+    # These are the 2.0 question definitions
+    ###
+    # phs_cover_1a1 = Q_Radio(3301625, "Clinical trial (radio)")
+    # phs_cover_1a2 = Q_Radio(3301628, "Phase III Clinical trial (radio)")
+    # phs_cover_2 = Q_Radio(3301663, "Disclosure permission (radio)")
+    # phs_cover_cell_line = Q_Text(3301640, "Cell line (text)")
+    # # TODO - add checkbox!
+    # phs_cover_4 = Q_Radio(3301668, "Inventions and Patents (radio)")
+    # phs_cover_4a = Q_Radio(3301670, "Inventions previously reported (radio)")
+    # phs_cover_5 = Q_Radio(3301648, "Change of investigator (radio)")
+    # phs_cover_5a_prefix = Q_Select(3301650, "Prefix (dropdown)")
+    # phs_cover_5a_firstname = Q_Text(3301651, "First name (text)")
+    # phs_cover_5a_middlename = Q_Text(3301652, "Middle name (text)")
+    # phs_cover_5a_lastname = Q_Text(3301653, "Last name (text)")
+    # phs_cover_5a_suffix = Q_Select(3301654, "Suffix (dropdown)")
+    # phs_cover_6 = Q_Radio(3301655, "Change of institution (radio)")
+    # phs_cover_6a = Q_Text(3301656, "Institution name (text)")
 
     fellowship_4c = Q_Select(3301530, "Field of study (dropdown)")
     fellowship_4d = Q_Radio(3301531, "Prior support (radio)")
@@ -115,7 +139,28 @@ class SCR0612b(Page):
     fellowship_5 = Q_Radio(3301553, "Change of institution (radio)")
     fellowship_6a = Q_Radio(3301559, "Tuition and fees (radio)")
 
-    career_1 = Q_Radio(3301377, "Citizenship (radio)")
+    career_1 = Q_Radio(3301696, "Citizenship (radio)")
+    career_1a = Q_Radio(3301699, "Citizenship subquestion (radio)")
+    career_1b = Q_Select(3301703, "Citizenship visa (select)")
+
+    assign_award1 = Q_Text(3301707, "Assign to Awarding Component (1) (text)")
+    assign_award2 = Q_Text(3301708, "Assign to Awarding Component (2) (text)")
+    assign_award3 = Q_Text(3301709, "Assign to Awarding Component (3) (text)")
+    assign_award4 = Q_Text(3301710, "Do not Assign to Awarding Component (1) (text)")
+    assign_award5 = Q_Text(3301711, "Do not Assign to Awarding Component (2) (text)")
+    assign_award6 = Q_Text(3301712, "Do not Assign to Awarding Component (3) (text)")
+    assign_study1 = Q_Text(3301717, "Assign to Study Section (1) (text)")
+    assign_study2 = Q_Text(3301718, "Assign to Study Section (2) (text)")
+    assign_study3 = Q_Text(3301719, "Assign to Study Section (3) (text)")
+    assign_study4 = Q_Text(3301720, "Do not Assign to Study Section (1) (text)")
+    assign_study5 = Q_Text(3301721, "Do not Assign to Study Section (2) (text)")
+    assign_study6 = Q_Text(3301722, "Do not Assign to Study Section (3) (text)")
+    assign_individuals = Q_Text(3301724, "Individuals not to assign (text)")
+    assign_expertise1 = Q_Text(3301727, "Expertise (1) (text)")
+    assign_expertise2 = Q_Text(3301728, "Expertise (2) (text)")
+    assign_expertise3 = Q_Text(3301729, "Expertise (3) (text)")
+    assign_expertise4 = Q_Text(3301730, "Expertise (4) (text)")
+    assign_expertise5 = Q_Text(3301731, "Expertise (5) (text)")
 
     def add_ps(self):
         """
